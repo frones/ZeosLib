82f889299cee96f857e7d28fb331ed5cd8db773f
helly <helly@d681b350-854c-439c-a81d-c0ce20710203>
2006-10-14 00:56:39 +0000

    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/merge_help@132 d681b350-854c-439c-a81d-c0ce20710203

--- src/plain/ZPlainPostgreSql8.pas
+++ src/plain/ZPlainPostgreSql8.pas
@@ -57,8 +57,8 @@ const
 {$IFNDEF STRICT_DLL_LOADING}
   WINDOWS2_DLL_LOCATION   = 'libpq.dll';
 {$ENDIF}
-  LINUX_DLL_LOCATION   = 'libpq.so';
-  LINUX_DLL_LOCATION2  = 'libpq.so.4';
+  LINUX1_DLL_LOCATION  = 'libpq.so.4';
+  LINUX2_DLL_LOCATION   = 'libpq.so';
 
 { Type Lengths }
   NAMEDATALEN  = 32;
@@ -488,7 +488,7 @@ initialization
 {$ENDIF}
     ]);
 {$ELSE}
-  LibraryLoader := TZPostgreSQLNativeLibraryLoader.Create([LINUX_DLL_LOCATION,LINUX_DLL_LOCATION2]);
+  LibraryLoader := TZPostgreSQLNativeLibraryLoader.Create([LINUX1_DLL_LOCATION,LINUX2_DLL_LOCATION]);
 {$ENDIF}
 finalization
   if Assigned(LibraryLoader) then
