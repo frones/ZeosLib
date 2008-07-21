7955198f80c052dcb4cea94896154d68f94bb588
mdaems <mdaems@d681b350-854c-439c-a81d-c0ce20710203>
2008-07-21 18:45:18 +0000

    Updated PostgreSQL keywords list - by pawelsel - Mantis Bug 122
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing@392 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcPostgreSqlMetadata.pas
+++ src/dbc/ZDbcPostgreSqlMetadata.pas
@@ -421,11 +421,41 @@ end;
 }
 function TZPostgreSQLDatabaseMetadata.GetSQLKeywords: string;
 begin
-  Result := 'abort,acl,add,aggregate,append,archive,arch_store,backward,binary,change,'+
-            'cluster,copy,database,delimiter,delimiters,do,extend,explain,forward,heavy,'+
-            'index,inherits,isnull,light,listen,load,merge,nothing,notify,notnull,oids,'+
-            'purge,rename,replace,retrieve,returns,rule,recipe,setof,stdin,stdout,store,'+
-            'vacuum,verbose,version,user';
+  Result := 'abort,absolute,access,action,add,admin,after,aggregate,all,also,alter,'+
+            'always,analyse,analyze,and,any,array,asc,assertion,assignment,asymmetric,'+
+            'at,authorization,backward,before,begin,between,bigint,binary,bit,boolean,'+
+            'both,cache,called,cascade,cascaded,case,cast,chain,character,characteristics,'+
+            'check,checkpoint,class,close,cluster,coalesce,collate,column,comment,commit,'+
+            'committed,concurrently,configuration,connect,connection,constraint,constraints,'+
+            'content,conversion,convert,copy,cost,createdb,createrole,createuser,cross,csv,'+
+            'current,current_date,current_role,current_time,current_timestamp,current_user,'+
+            'cursor,cycle,database,day,deallocate,dec,decimal,declare,default,defaults,'+
+            'deferrable,deferred,definer,delimiter,delimiters,desc,dictionary,disable,'+
+            'discard,distinct,do,document,domain,double,each,else,enable,encoding,encrypted,'+
+            'end,end-exec,enum,escape,except,excluding,exclusive,exec,execute,exists,explain,'+
+            'external,extract,false,family,fetch,first,float,for,force,foreign,forward,'+
+            'freeze,full,function,global,grant,granted,greatest,handler,header,hold,hour,'+
+            'if,ilike,immediate,immutable,implicit,in,including,increment,indexes,inherit,'+
+            'inherits,initially,inner,inout,input,insensitive,instead,int,intersect,interval,'+
+            'invoker,isnull,isolation,join,lancompiler,language,large,last,leading,least,left,'+
+            'level,like,limit,listen,load,local,localtime,localtimestamp,location,lock,'+
+            'login,mapping,match,maxvalue,minute,minvalue,mode,month,move,name,names,national,'+
+            'natural,nchar,new,next,no,nocreatedb,nocreaterole,nocreateuser,noinherit,nologin,'+
+            'none,nosuperuser,not,nothing,notify,notnull,nowait,nullif,nulls,numeric,object,'+
+            'of,off,offset,oids,old,only,operator,option,or,out,outer,overlaps,overlay,owned,'+
+            'owner,parser,partial,password,placing,plans,position,precision,prepare,prepared,'+
+            'preserve,prior,privileges,procedural,procedure,quote,read,real,reassign,recheck,'+
+            'references,reindex,relative,release,rename,repeatable,replace,replica,reset,'+
+            'restart,restrict,return,returning,returns,revoke,right,role,rollback,row,rows,'+
+            'rule,savepoint,schema,scroll,search,second,security,sequence,serializable,session,'+
+            'session_user,setof,share,show,similar,simple,smallint,some,stable,standalone,'+
+            'start,statement,statistics,stdin,stdout,storage,strict,strip,substring,'+
+            'superuser,symmetric,sysid,system,tablespace,temp,template,temporary,text,'+
+            'then,time,timestamp,to,trailing,transaction,treat,trigger,trim,true,truncate,'+
+            'trusted,type,uncommitted,unencrypted,union,unique,unknown,unlisten,until,user,'+
+            'using,vacuum,valid,validator,value,varying,verbose,version,view,volatile,when,'+
+            'whitespace,with,without,work,write,xml,xmlattributes,xmlconcat,xmlelement,'+
+            'xmlforest,xmlparse,xmlpi,xmlroot,xmlserialize,year,yes,zone';
 end;
 
 {**
