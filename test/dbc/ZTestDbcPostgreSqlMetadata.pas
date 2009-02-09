4f4244a98bb820bed0fcddc65f67d847f1ad36f7
mdaems <mdaems@d681b350-854c-439c-a81d-c0ce20710203>
2009-02-09 21:36:20 +0000

    Fixed broken IdentifierQuoting tests - by mdaems
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing@588 d681b350-854c-439c-a81d-c0ce20710203

--- test/dbc/ZTestDbcPostgreSqlMetadata.pas
+++ test/dbc/ZTestDbcPostgreSqlMetadata.pas
@@ -92,6 +92,7 @@ type
     procedure TestGetCrossReference;
     procedure TestGetTypeInfo;
     procedure TestGetIndexInfo;
+    procedure TestIdentifierQuoting;
   end;
 
 implementation
@@ -771,6 +772,12 @@ begin
     Close;
   end;
   ResultSet := nil;
+end;
+
+procedure TZTestPostgreSqlMetadataCase.TestIdentifierQuoting;
+begin
+      Check(Metadata.GetIdentifierConvertor.Quote('A9A')=Metadata.GetDatabaseInfo.GetIdentifierQuoteString[1]+'A9A'+Metadata.GetDatabaseInfo.GetIdentifierQuoteString[length(Metadata.GetDatabaseInfo.GetIdentifierQuoteString)]);
+      Check(Metadata.GetIdentifierConvertor.Quote('a9A')=Metadata.GetDatabaseInfo.GetIdentifierQuoteString[1]+'a9A'+Metadata.GetDatabaseInfo.GetIdentifierQuoteString[length(Metadata.GetDatabaseInfo.GetIdentifierQuoteString)]);
 end;
 
 initialization
