cfd95e88eee372af626d399e1f9f447b435b530e
markdaems <markdaems@d681b350-854c-439c-a81d-c0ce20710203>
2011-08-30 14:54:35 +0000

    Addition to 'Patch fix for Postgresql,better Indexnames precision' - by aebi - See http://zeos.firmos.at/viewtopic.php?t=3261
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing@926 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcPostgreSqlResultSet.pas
+++ src/dbc/ZDbcPostgreSqlResultSet.pas
@@ -185,8 +185,11 @@ begin
 
   case TypeOid of
     790: ColumnInfo.Currency := True; { money }
-    19: if ((Connection.GetServerMajorVersion = 7) and (Connection.GetServerMinorVersion < 3)) then ColumnInfo.Precision := 32
-        else ColumnInfo.Precision := 64; { name }
+    19: if (Connection.GetServerMajorVersion < 7) or
+           ((Connection.GetServerMajorVersion = 7) and (Connection.GetServerMinorVersion < 3)) then
+          ColumnInfo.Precision := 32
+        else
+          ColumnInfo.Precision := 64; { name }
     1186: ColumnInfo.Precision := 32; { interval }
     24: ColumnInfo.Precision := 10; { regproc }
     17:{ bytea }
