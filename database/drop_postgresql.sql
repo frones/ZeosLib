16c64b743a9784292e4d78eb135d8eb669b173e5
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2012-02-20 22:22:03 +0000

    Changed PostgreSQL drop_postgresql.sql
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing@1025 d681b350-854c-439c-a81d-c0ce20710203

--- database/drop_postgresql.sql
+++ database/drop_postgresql.sql
@@ -21,10 +21,11 @@ drop table case_sensitive;
 drop table high_load;
 
 drop function procedure1(INT4);
---drop function procedure2();
-
+drop function procedure2();
+/*
 drop SEQUENCE cargo_c_id_seq;
 drop SEQUENCE default_values_d_id_seq;
 drop SEQUENCE department_dep_id_seq;
 drop SEQUENCE equipment_eq_id_seq;
 drop SEQUENCE people_p_id_seq;
+*/
