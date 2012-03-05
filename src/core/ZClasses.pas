0fa0f81e7487beccf6c7b49fce82ada7bbdb6f65
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2012-03-05 16:17:57 +0000

    removed WITH_NEWOBBJECT define from GetHashCode
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing@1057 d681b350-854c-439c-a81d-c0ce20710203

--- src/core/ZClasses.pas
+++ src/core/ZClasses.pas
@@ -200,7 +200,7 @@ type
   TZAbstractObject = class(TInterfacedObject, IZObject)
   public
     function Equals(const Value: IZInterface): Boolean; {$IFDEF WITH_NEWTOBJECT}overload;{$ENDIF} virtual;
-    {$IFNDEF WITH_NEWTOBJECT}function GetHashCode: LongInt;{$ENDIF}
+    function GetHashCode: LongInt;
     function Clone: IZInterface; virtual;
     function ToString: string;{$IFDEF WITH_NEWTOBJECT}override{$ELSE} virtual{$ENDIF} ;
     function InstanceOf(const IId: TGUID): Boolean;
@@ -284,12 +284,10 @@ end;
   Gets a unique hash for this object.
   @return a unique hash for this object.
 }
-{$IFNDEF WITH_NEWTOBJECT}
 function TZAbstractObject.GetHashCode: LongInt;
 begin
   Result := LongInt(Self);
 end;
-{$ENDIF}
 
 {**
   Clones an object instance.
