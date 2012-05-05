b6200afea571e543c124c63c3601484733f54499
markdaems <markdaems@d681b350-854c-439c-a81d-c0ce20710203>
2012-05-05 22:06:18 +0000

    Added artwork sources from ism - by ism - see http://zeos.firmos.at/viewtopic.php?t=3420 - Removed Old Design (That's versioned in svn anyway) - Made 1 build script for Delphi and Lazarus resources that overwrites files in src/components - Removed intermediate files - Added resource compiler (Microsoft SDK rc.exe)
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-egonhugeist@1242 d681b350-854c-439c-a81d-c0ce20710203

---
+++ artwork/make_resources.bat
@@ -0,0 +1,28 @@
+@@echo off
+echo ----------------------
+echo BUILD Delphi resources
+echo ----------------------
+rc.exe -r -v -fo../src/component/ZComponent.dcr ZComponent.rc
+echo -----------------------
+echo BUILD Lazarus resources
+echo -----------------------
+bmptoxpm TZConnection.bmp
+bmptoxpm TZReadOnlyQuery.bmp
+bmptoxpm TZQuery.bmp
+bmptoxpm TZUpdateSql.bmp
+bmptoxpm TZSqlProcessor.bmp
+bmptoxpm TZTable.bmp
+bmptoxpm TZStoredProc.bmp
+bmptoxpm TZSqlMonitor.bmp
+bmptoxpm TZSqlMetaData.bmp
+bmptoxpm TZSequence.bmp
+bmptoxpm TZIBEventAlerter.bmp
+bmptoxpm TZConnectionGroup.bmp
+bmptoxpm TZGroupedConnection.bmp
+bmptoxpm TZPgEventAlerter.bmp
+lazres ../src/component/ZComponentReg.lrs TZConnection.xpm TZReadOnlyQuery.xpm TZConnectionGroup.xpm TZPgEventAlerter.xpm TZGroupedConnection.xpm TZQuery.xpm TZUpdateSql.xpm TZSqlProcessor.xpm TZTable.xpm TZStoredProc.xpm TZSqlMonitor.xpm TZSqlMetaData.xpm TZSequence.xpm TZIBEventAlerter.xpm
+echo ----------------------
+echo Remove temporary files
+echo ----------------------
+del *.xpm
+pause
