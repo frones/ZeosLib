71c75f1415c87489e75c5fad6f8b52fb5d07850b
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2012-06-10 22:11:05 +0000

    fixed: MantisBug #0000253 The table name is interpreted as the name of the database.
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-egonhugeist@1385 d681b350-854c-439c-a81d-c0ce20710203

--- src/component/ZAbstractConnection.pas
+++ src/component/ZAbstractConnection.pas
@@ -1224,7 +1224,7 @@ end;
 }
 procedure TZAbstractConnection.GetTableNames(const Pattern: string; List: TStrings);
 begin
-  GetTableNames('',Pattern,nil,List);
+  GetTableNames(Pattern,'',nil,List);
 end;
 
 {**
