e27317af21f12fb74389f94f6f04747666f519c8
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2012-06-28 21:04:11 +0000

    fixed missing ftShortInt for D12_UP
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-egonhugeist@1423 d681b350-854c-439c-a81d-c0ce20710203

--- src/component/ZAbstractRODataset.pas
+++ src/component/ZAbstractRODataset.pas
@@ -1111,7 +1111,7 @@ begin
         case Param.DataType of
           ftBoolean:
             Statement.SetBoolean(I + 1, Param.AsBoolean);
-          ftSmallInt:
+          ftSmallInt, ftShortInt:
             Statement.SetShort(I + 1, Param.AsSmallInt);
           ftInteger, ftAutoInc:
             Statement.SetInt(I + 1, Param.AsInteger);
