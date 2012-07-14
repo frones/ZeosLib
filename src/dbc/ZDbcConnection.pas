90e86c93817a2f84a8c7b1fcedd58f109a53c344
ludob <ludob@d681b350-854c-439c-a81d-c0ce20710203>
2012-07-14 15:30:03 +0000

    Fixed leak in TZAbstractDriver.Connect when failing connection. Solves also Mysql TEST924861.
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-egonhugeist@1515 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcConnection.pas
+++ src/dbc/ZDbcConnection.pas
@@ -360,8 +360,11 @@ var
   TempURL:  TZURL;
 begin
   TempURL := TZURL.Create(Url, Info);
-  Result := Connect(TempURL);
-  TempUrl.Free;
+  try
+    Result := Connect(TempURL);
+  finally
+    TempUrl.Free;
+  end;
 end;
 {$WARNINGS ON}
 
