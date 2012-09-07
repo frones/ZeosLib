91a72488f915a733914f5ea0c104674790a495df
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2012-09-07 12:57:45 +0000

    added TEST_All_TYPES to create scripts
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing@1735 d681b350-854c-439c-a81d-c0ce20710203

--- database/drop_mysql.sql
+++ database/drop_mysql.sql
@@ -5,6 +5,7 @@
 /*==============================================================*/
 
 drop procedure IF EXISTS ABTEST;
+drop procedure IF EXISTS TEST_All_TYPES;
 drop table IF EXISTS blob_values;
 drop table IF EXISTS date_values;
 drop table IF EXISTS default_values;
