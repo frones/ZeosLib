367ba39700a1c5c7efeb5fe40afa18a421a98531
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2012-09-24 09:56:09 +0000

    fixed ASA12 binary reading issue with MBCS character sets
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing@1848 d681b350-854c-439c-a81d-c0ce20710203

--- src/plain/ZPlainASAConstants.pas
+++ src/plain/ZPlainASAConstants.pas
@@ -798,7 +798,7 @@ type
     array_len        : Longword;
     stored_len       : Longword;
     untrunc_len      : Longword;
-    arr: array[0..0] of AnsiChar;
+    arr: array[0..312756] of AnsiChar;
   end;
 
   ZASA_db_callback_index = (             //Markus
