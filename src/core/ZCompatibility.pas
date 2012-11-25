8628e7da3eb5daee33406b1f0318f9361d42c095
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2012-11-25 21:35:45 +0000

    Merged revision(s) 2043, 2047 from branches/testing-7.1: fixed remaining compiling issues on *nix FPC2.6down (self tested) ........ found and fixed last compiling issue for FPC2.7.1 and Linux systems: global define FPC_HAS_BUILTIN_WIDESTR_MANAGER is NOT declared there! Why? So i check if it's defined -> redefine it! ........
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing@2049 d681b350-854c-439c-a81d-c0ce20710203

--- src/core/ZCompatibility.pas
+++ src/core/ZCompatibility.pas
@@ -459,7 +459,11 @@ begin
                       Move(PAnsiChar(TempAnsi)^, PAnsiChar(Result)^, Length(TempAnsi));
                     end;
                     {$ELSE}
-                    Result := AnsiToStringEx(Ansi, zCP_UTF8, ConSettings.CTRL_CP);
+                      {$IFDEF WITH_LCONVENCODING}
+                      Result := Ansi;
+                      {$ELSE}
+                      Result := AnsiToStringEx(Ansi, zCP_UTF8, ConSettings.CTRL_CP);
+                      {$ENDIF}
                     {$ENDIF}
               end
             else
@@ -660,7 +664,11 @@ begin
                 Move(PAnsiChar(TempAnsi)^, PAnsiChar(Result)^, Length(TempAnsi));
               end
               {$ELSE}
-              Result := StringToAnsiEx(AStr, ConSettings.CTRL_CP, zCP_UTF8)
+                {$IFDEF WITH_LCONVENCODING}
+                Result := AnsiToUTF8(AStr)
+                {$ELSE}
+                Result := StringToAnsiEx(AStr, ConSettings.CTRL_CP, zCP_UTF8)
+                {$ENDIF}
               {$ENDIF}
         else
           Result := AStr;
