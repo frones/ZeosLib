c86c546be3f263f74927923cb9d7adcf8bc2ed0d
markdaems <markdaems@d681b350-854c-439c-a81d-c0ce20710203>
2013-01-01 19:23:19 +0000

    Final version update in 7.0 testing branch. Today we release the stable version and make a Stable maintanence branch.
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing@2090 d681b350-854c-439c-a81d-c0ce20710203

--- src/core/ZClasses.pas
+++ src/core/ZClasses.pas
@@ -61,9 +61,9 @@ uses
 const
   ZEOS_MAJOR_VERSION = 7;
   ZEOS_MINOR_VERSION = 0;
-  ZEOS_SUB_VERSION = 2;
-  ZEOS_STATUS = 'rc';
-  ZEOS_VERSION = '7.0.2-rc';
+  ZEOS_SUB_VERSION = 3;
+  ZEOS_STATUS = 'stable';
+  ZEOS_VERSION = '7.0.3-stable';
 {$IFDEF ENABLE_POOLED}
   {Pooled Protocol Prefix, including final dot}
   PooledPrefix = 'pooled.';
