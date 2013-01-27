0928ea08ea4aa2b76f2b66378a2dc04821098df0
markdaems <markdaems@d681b350-854c-439c-a81d-c0ce20710203>
2013-01-27 16:48:04 +0000

    Merged revision(s) 2134, 2139-2140, 2145 from branches/testing-7.1: - *7.0* Wrong DisplaySize alignment. Fields have been truncated. See http://zeos.firmos.at/viewtopic.php?p=16836#16836 - ? 7.0 ? defines cleanup for DELPHi12_UP on component-layer (replaced with UNICODE) - omit preferpreparedresolver from ZDataSet options and resolver. Equal behavior if only doPreferPrepared is set. - Remove preferpreparedresolver from test suite
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/trunk@2150 d681b350-854c-439c-a81d-c0ce20710203

--- src/component/ZAbstractRODataset.pas
+++ src/component/ZAbstractRODataset.pas
@@ -75,8 +75,7 @@ type
 
   {** Options for dataset. }
   TZDatasetOption = (doOemTranslate, doCalcDefaults, doAlwaysDetailResync,
-    doSmartOpen, doPreferPrepared, doPreferPreparedResolver, doDontSortOnPost,
-    doUpdateMasterFirst);
+    doSmartOpen, doPreferPrepared, doDontSortOnPost, doUpdateMasterFirst);
 
   {** Set of dataset options. }
   TZDatasetOptions = set of TZDatasetOption;
@@ -1660,10 +1659,6 @@ begin
       Temp.Values['preferprepared'] := 'true'
     else
       Temp.Values['preferprepared'] := 'false';
-    if doPreferPreparedResolver in FOptions then
-      Temp.Values['preferpreparedresolver'] := 'true'
-    else
-      Temp.Values['preferpreparedresolver'] := 'false';
 
     Result := FConnection.DbcConnection.PrepareStatementWithParams(SQL, Temp);
   finally
@@ -1754,7 +1749,7 @@ begin
       for i := 0 to Fields.Count -1 do
         if Fields[i].DataType in [ftString, ftWideString] then
           if not (ResultSet.GetMetadata.GetColumnDisplaySize(I+1) = 0) then
-            Fields[i].Size := ResultSet.GetMetadata.GetColumnDisplaySize(I+1);
+            Fields[i].DisplayWidth := ResultSet.GetMetadata.GetColumnDisplaySize(I+1);
     end;
     BindFields(True);
 
