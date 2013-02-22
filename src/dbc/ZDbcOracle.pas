c885bce8b83062df4c41454d833b7d42dbaf64ea
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2013-02-22 10:42:32 +0000

    *7.0* fix nasty Exception for Oracle GetCatalog-MetaInfos on TZPropertyEditor
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.1@2188 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcOracle.pas
+++ src/dbc/ZDbcOracle.pas
@@ -277,6 +277,9 @@ end;
 }
 destructor TZOracleConnection.Destroy;
 begin
+  if not IsClosed then
+     Close;
+
   if FHandle <> nil then
   begin
     GetPlainDriver.HandleFree(FHandle, OCI_HTYPE_ENV);
@@ -380,7 +383,6 @@ begin
   end;
   GetPlainDriver.AttrSet(FContextHandle, OCI_HTYPE_SVCCTX, FSessionHandle, 0,
     OCI_ATTR_SESSION, FErrorHandle);
-
   DriverManager.LogMessage(lcConnect, PlainDriver.GetProtocol, LogMessage);
 
   StartTransactionSupport;
