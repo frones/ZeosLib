588345c45c57e276aa00cced607ff6f0454107f1
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2013-05-20 19:06:09 +0000

    *7.0* added failing FireBird test for decimal values. (NOT numeric type)
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.1@2230 d681b350-854c-439c-a81d-c0ce20710203

--- test/bugreport/ZTestBugCompInterbase.pas
+++ test/bugreport/ZTestBugCompInterbase.pas
@@ -81,6 +81,7 @@ type
     procedure Test984305;
     procedure Test1004584;
     procedure Test1021705;
+    procedure Test_Decimal;
   end;
 
   ZTestCompInterbaseBugReportMBCs = class(TZAbstractCompSQLTestCaseMBCs)
@@ -172,6 +173,129 @@ begin
 
   finally
     Query.Free;
+  end;
+end;
+
+{
+  Rounding issues:
+  http://zeos.firmos.at/viewtopic.php?t=3589
+  http://zeos.firmos.at/viewtopic.php?t=3725
+}
+procedure ZTestCompInterbaseBugReport.Test_Decimal;
+const
+  Row1_Num2 = '541.77';
+  Row1_Num3 = '541.777';
+  Row1_Num4 = '541.7777';
+  Row1_Num5 = '541.77777';
+  Row1_Num6 = '541.777777';
+  Row1_Num7 = '541.7777777';
+  Row1_Num8 = '541.77777777';
+
+  Row2_Num2 = '541.74';
+  Row2_Num3 = '541.774';
+  Row2_Num4 = '541.7774';
+  Row2_Num5 = '541.77774';
+  Row2_Num6 = '541.777774';
+  Row2_Num7 = '541.7777774';
+  Row2_Num8 = '541.77777774';
+
+  Row3_Num2 = '23.45';
+  Row3_Num3 = '23.445';
+  Row3_Num4 = '23.4445';
+  Row3_Num5 = '23.44445';
+  Row3_Num6 = '23.444445';
+  Row3_Num7 = '23.4444445';
+  Row3_Num8 = '23.44444445';
+var
+  Table: TZTable;
+  DecimalSep: Char;
+begin
+  if SkipForReason(srClosedBug) then Exit;
+
+  Table := CreateTable;
+  DecimalSep := {$IFDEF WITH_FORMATSETTINGS}FormatSettings.{$ENDIF}DecimalSeparator;
+  {$IFDEF WITH_FORMATSETTINGS}FormatSettings.{$ENDIF}DecimalSeparator := '.';
+  try
+    // Query.RequestLive := True;
+    Table.TableName := 'TEST_DECIMAL';
+    Table.Open;
+
+    Table.Append;
+    Table.FieldByName('ID').AsInteger := 1;
+    Table.FieldByName('NUM_2').AsString := Row1_Num2;
+    Table.FieldByName('NUM_3').AsString := Row1_Num3;
+    Table.FieldByName('NUM_4').AsString := Row1_Num4;
+    Table.FieldByName('NUM_5').AsString := Row1_Num5;
+    Table.FieldByName('NUM_6').AsString := Row1_Num6;
+    Table.FieldByName('NUM_7').AsString := Row1_Num7;
+    Table.FieldByName('NUM_8').AsString := Row1_Num8;
+    Table.Post;
+
+    Table.Append;
+    Table.FieldByName('ID').AsInteger := 2;
+    Table.FieldByName('NUM_2').AsString := Row2_Num2;
+    Table.FieldByName('NUM_3').AsString := Row2_Num3;
+    Table.FieldByName('NUM_4').AsString := Row2_Num4;
+    Table.FieldByName('NUM_5').AsString := Row2_Num5;
+    Table.FieldByName('NUM_6').AsString := Row2_Num6;
+    Table.FieldByName('NUM_7').AsString := Row2_Num7;
+    Table.FieldByName('NUM_8').AsString := Row2_Num8;
+    Table.Post;
+
+    Table.Append;
+    Table.FieldByName('ID').AsInteger := 3;
+    Table.FieldByName('NUM_2').AsString := Row3_Num2;
+    Table.FieldByName('NUM_3').AsString := Row3_Num3;
+    Table.FieldByName('NUM_4').AsString := Row3_Num4;
+    Table.FieldByName('NUM_5').AsString := Row3_Num5;
+    Table.FieldByName('NUM_6').AsString := Row3_Num6;
+    Table.FieldByName('NUM_7').AsString := Row3_Num7;
+    Table.FieldByName('NUM_8').AsString := Row3_Num8;
+    Table.Post;
+    Table.Close;
+
+    Table.Open;
+
+    CheckEquals(1, Table.FieldByName('ID').AsInteger);
+    CheckEquals(Row1_Num2, Table.FieldByName('NUM_2').AsString);
+    CheckEquals(Row1_Num3, Table.FieldByName('NUM_3').AsString);
+    CheckEquals(Row1_Num4, Table.FieldByName('NUM_4').AsString);
+    CheckEquals(Row1_Num5, Table.FieldByName('NUM_5').AsString);
+    CheckEquals(Row1_Num6, Table.FieldByName('NUM_6').AsString);
+    CheckEquals(Row1_Num7, Table.FieldByName('NUM_7').AsString);
+    CheckEquals(Row1_Num8, Table.FieldByName('NUM_8').AsString);
+
+    Table.Next;
+
+    CheckEquals(2, Table.FieldByName('ID').AsInteger);
+    CheckEquals(Row2_Num2, Table.FieldByName('NUM_2').AsString);
+    CheckEquals(Row2_Num3, Table.FieldByName('NUM_3').AsString);
+    CheckEquals(Row2_Num4, Table.FieldByName('NUM_4').AsString);
+    CheckEquals(Row2_Num5, Table.FieldByName('NUM_5').AsString);
+    CheckEquals(Row2_Num6, Table.FieldByName('NUM_6').AsString);
+    CheckEquals(Row2_Num7, Table.FieldByName('NUM_7').AsString);
+    CheckEquals(Row2_Num8, Table.FieldByName('NUM_8').AsString);
+
+    Table.Next;
+
+    CheckEquals(2, Table.FieldByName('ID').AsInteger);
+    CheckEquals(Row3_Num2, Table.FieldByName('NUM_2').AsString);
+    CheckEquals(Row3_Num3, Table.FieldByName('NUM_3').AsString);
+    CheckEquals(Row3_Num4, Table.FieldByName('NUM_4').AsString);
+    CheckEquals(Row3_Num5, Table.FieldByName('NUM_5').AsString);
+    CheckEquals(Row3_Num6, Table.FieldByName('NUM_6').AsString);
+    CheckEquals(Row3_Num7, Table.FieldByName('NUM_7').AsString);
+    CheckEquals(Row3_Num8, Table.FieldByName('NUM_8').AsString);
+
+  finally
+    try
+      Table.Delete;
+      Table.Delete;
+      Table.Delete;
+    finally
+      {$IFDEF WITH_FORMATSETTINGS}FormatSettings.{$ENDIF}DecimalSeparator := DecimalSep;
+      Table.Free;
+    end;
   end;
 end;
 
