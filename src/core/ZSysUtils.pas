1df55d9f1ec35ce3b7a395613759da3592a524df
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2013-06-15 20:27:36 +0000

    *7.1* fix for *7.* Added a ZSetString overload for ZWideString alias and enabled Convert of vtBytes to vtString
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.1@2334 d681b350-854c-439c-a81d-c0ce20710203

--- src/core/ZSysUtils.pas
+++ src/core/ZSysUtils.pas
@@ -398,6 +398,7 @@ procedure ZSetString(const Src: PAnsiChar; var Dest: AnsiString); overload;
 procedure ZSetString(const Src: PAnsiChar; const Len: Cardinal; var Dest: AnsiString); overload;
 procedure ZSetString(const Src: PAnsiChar; var Dest: UTF8String); overload;
 procedure ZSetString(const Src: PAnsiChar; const Len: Cardinal; var Dest: UTF8String); overload;
+procedure ZSetString(const Src: PAnsiChar; const Len: Cardinal; var Dest: ZWideString); overload;
 {$IFDEF WITH_RAWBYTESTRING}
 procedure ZSetString(const Src: PAnsiChar; var Dest: RawByteString); overload;
 procedure ZSetString(const Src: PAnsiChar; const Len: Cardinal; var Dest: RawByteString); overload;
@@ -1528,6 +1529,17 @@ begin
   begin
     SetLength(Dest, Len);
     Move(Src^, PAnsiChar(Dest)^, Len);
+  end;
+end;
+
+procedure ZSetString(const Src: PAnsiChar; const Len: Cardinal; var Dest: ZWideString); overload;
+begin
+  if ( Len = 0 ) or ( Src = nil ) then
+    Dest := ''
+  else
+  begin
+    SetLength(Dest, Len div 2);
+    Move(Src^, PWideChar(Dest)^, Len);
   end;
 end;
 
