76ee4ae7f1fb7e44cbfad8f936f3a477341dd041
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2013-07-25 13:13:11 +0000

    *7.0* omit handle_indeterminate_datatype option. Handle this case always. Start going to create an Abstraction-Class.
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.1@2550 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcPostgreSqlStatement.pas
+++ src/dbc/ZDbcPostgreSqlStatement.pas
@@ -154,9 +154,7 @@ type
     FPQparamLengths: TPQparamLengths;
     FPQparamFormats: TPQparamFormats;
     Findeterminate_datatype: Boolean;
-    FHandle_indeterminate_datatype: Boolean;
     Foidasblob: Boolean;
-    Fdeterminate_datatype_failed: Boolean;
     function CreateResultSet(QueryHandle: PZPostgreSQLResult): IZResultSet;
     function ExectuteInternal(const SQL: RawByteString; const LogSQL: String;
       const LoggingCategory: TZLoggingCategory): PZPostgreSQLResult;
@@ -234,10 +232,8 @@ begin
   ResultSetType := rtScrollInsensitive;
 
   { Processes connection properties. }
-  if Self.Info.Values['oidasblob'] <> '' then
-    FOidAsBlob := StrToBoolEx(Self.Info.Values['oidasblob'])
-  else
-    FOidAsBlob := (Connection as IZPostgreSQLConnection).IsOidAsBlob;
+  FOidAsBlob := StrToBoolEx(Self.Info.Values['oidasblob'])
+    or (Connection as IZPostgreSQLConnection).IsOidAsBlob;
 end;
 
 {**
@@ -870,10 +866,9 @@ begin
       begin
         Result := FPlainDriver.Prepare(FConnectionHandle, PAnsiChar(RawByteString(FPlanName)),
           PAnsiChar(SQL), InParamCount, nil);
-        Fdeterminate_datatype_failed := (CheckPostgreSQLError(Connection, FPlainDriver,
+        Findeterminate_datatype := (CheckPostgreSQLError(Connection, FPlainDriver,
           FPostgreSQLConnection.GetConnectionHandle, LoggingCategory, LogSQL,
-            Result, FHandle_indeterminate_datatype) = '42P18');
-        Findeterminate_datatype := FHandle_indeterminate_datatype and Fdeterminate_datatype_failed;
+            Result) = '42P18');
         DriverManager.LogMessage(LoggingCategory, FPlainDriver.GetProtocol, LogSQL);
         if not Findeterminate_datatype then
           FPostgreSQLConnection.RegisterPreparedStmtName(FPlanName);
@@ -1080,7 +1075,6 @@ constructor TZPostgreSQLCAPIPreparedStatement.Create(PlainDriver: IZPostgreSQLPl
   Connection: IZPostgreSQLConnection; const SQL: string; Info: TStrings);
 begin
   inherited Create(Connection, SQL, Info);
-  FHandle_indeterminate_datatype := StrToBoolEx(Self.Info.Values['handle_indeterminate_datatype']);
   Foidasblob := StrToBoolDef(Self.Info.Values['oidasblob'], False) or
     (Connection as IZPostgreSQLConnection).IsOidAsBlob;
   FPostgreSQLConnection := Connection;
@@ -1138,7 +1132,7 @@ begin
   if Prepared and Assigned(FPostgreSQLConnection) then
   begin
     inherited Unprepare;
-    if (not Fdeterminate_datatype_failed)  then
+    if (not Findeterminate_datatype)  then
     begin
       TempSQL := 'DEALLOCATE "'+FPlanName+'";';
       QueryHandle := ExectuteInternal(RawByteString(TempSQL), TempSQL, lcUnprepStmt);
