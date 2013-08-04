9e56370bea17674dfecdf3bc2e6bd3a09bc97df1
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2013-08-04 12:34:02 +0000

    *7.0* XE4 fix. Compiling was broken and some deprecated fixes. See Ticket #39 SF.Net
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.1@2605 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcPostgreSqlStatement.pas
+++ src/dbc/ZDbcPostgreSqlStatement.pas
@@ -211,7 +211,7 @@ implementation
 
 uses
   Types, ZMessages, ZDbcPostgreSqlResultSet, ZDbcPostgreSqlUtils, ZTokenizer,
-  ZEncoding;
+  ZEncoding{$IFDEF WITH_UNITANSISTRINGS}, AnsiStrings{$ENDIF};
 
 { TZPostgreSQLStatement }
 
@@ -923,7 +923,7 @@ var
     UpdateNull(Index);
 
     FPQparamValues[ParamIndex] := AllocMem(Length(Value) + 1);
-    StrCopy(FPQparamValues[Index], PAnsiChar(Value));
+    {$IFDEF WITH_STRPCOPY_DEPRECATED}AnsiStrings.{$ENDIF}StrCopy(FPQparamValues[Index], PAnsiChar(Value));
   end;
 
   procedure UpdateBinary(Value: Pointer; const Len, Index: Integer);
