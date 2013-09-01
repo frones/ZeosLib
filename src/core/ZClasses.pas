8d1374e64baa10d2f675ae2b864e62765258ead2
markdaems <markdaems@d681b350-854c-439c-a81d-c0ce20710203>
2013-09-01 23:22:19 +0000

    Bump version to 7.0.5
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/7.0-patches@2713 d681b350-854c-439c-a81d-c0ce20710203

--- src/core/ZClasses.pas
+++ src/core/ZClasses.pas
@@ -61,9 +61,9 @@ uses
 const
   ZEOS_MAJOR_VERSION = 7;
   ZEOS_MINOR_VERSION = 0;
-  ZEOS_SUB_VERSION = 4;
+  ZEOS_SUB_VERSION = 5;
   ZEOS_STATUS = 'stable';
-  ZEOS_VERSION = '7.0.4-stable';
+  ZEOS_VERSION = '7.0.5-stable';
 {$IFDEF ENABLE_POOLED}
   {Pooled Protocol Prefix, including final dot}
   PooledPrefix = 'pooled.';
