b5ebffad0dd52b5579115367422b3bf77a4dff74
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2014-06-15 19:41:12 +0000

    clear some code for last quick fix
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.2@3155 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcOracleStatement.pas
+++ src/dbc/ZDbcOracleStatement.pas
@@ -98,8 +98,6 @@ type
     procedure Prepare; override;
     procedure Unprepare; override;
 
-    procedure ClearParameters; override;
-
     function ExecuteQueryPrepared: IZResultSet; override;
     function ExecuteUpdatePrepared: Integer; override;
     function ExecutePrepared: Boolean; override;
@@ -297,9 +295,6 @@ var
   Label SkipStringAssembling, StringAssembling;
 
 begin
-  if FInVars = nil then //EH: need deep check of subsequential run -> this shouldn't happen!
-    AllocateOracleSQLVars(FInVars, InParamCount);
-
   if (InParamCount > 0) and (InParamValues[0].VType = vtArray) then
   begin
     { array DML binding}
@@ -1060,7 +1055,7 @@ begin
         if InParamTypes[I] in [stString, stUnicodeString] then
         begin //point to data of string
           CharRec := ClientVarManager.GetAsCharRec(InParamValues[i], ConSettings^.ClientCodePage^.CP);
-          CurrentVar.Data := CharRec.P;
+          //CurrentVar.Data := CharRec.P;
           Status := FPlainDriver.BindByPos(FHandle, CurrentVar.BindHandle,
             FErrorHandle, I + 1, CharRec.P, CharRec.Len,
             CurrentVar.TypeCode, @CurrentVar.Indicator, @CurrentVar.DataSize,
@@ -1108,10 +1103,6 @@ begin
   end;
 end;
 
-procedure TZOraclePreparedStatement.ClearParameters;
-begin
-  inherited ClearParameters;
-end;
 {**
   Prepares an SQL statement
 }
@@ -1181,7 +1172,7 @@ begin
 
   { Unloads binded variables with values. }
   if ArrayCount = 0 then
-    UnloadOracleVars(FInVars, ArrayCount)
+    UnloadOracleVars(FInVars, 0)
   else
     UnloadOracleVars(FDescriptors, ArrayCount)
 
