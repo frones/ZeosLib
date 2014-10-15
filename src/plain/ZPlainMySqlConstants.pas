5f6260dcb45be18eed1669768ba12c49784301b9
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2014-10-15 20:20:17 +0000

    omit commented but twice defined code
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.2@3407 d681b350-854c-439c-a81d-c0ce20710203

--- src/plain/ZPlainMySqlConstants.pas
+++ src/plain/ZPlainMySqlConstants.pas
@@ -72,38 +72,6 @@ const
   MYSQL_PORT           = 3306;
   LOCAL_HOST           = 'localhost';
 
-{ Enum Field Types }
-{  FIELD_TYPE_DECIMAL   = 0;
-  FIELD_TYPE_TINY      = 1;
-  FIELD_TYPE_SHORT     = 2;
-  FIELD_TYPE_LONG      = 3;
-  FIELD_TYPE_FLOAT     = 4;
-  FIELD_TYPE_DOUBLE    = 5;
-  FIELD_TYPE_NULL      = 6;
-  FIELD_TYPE_TIMESTAMP = 7;
-  FIELD_TYPE_LONGLONG  = 8;
-  FIELD_TYPE_INT24     = 9;
-  FIELD_TYPE_DATE      = 10;
-  FIELD_TYPE_TIME      = 11;
-  FIELD_TYPE_DATETIME  = 12;
-  FIELD_TYPE_YEAR      = 13;
-  FIELD_TYPE_NEWDATE   = 14;
-  FIELD_TYPE_VARCHAR   = 15; //<--ADDED by fduenas 20-06-2006
-  FIELD_TYPE_BIT       = 16; //<--ADDED by fduenas 20-06-2006
-  FIELD_TYPE_NEWDECIMAL = 246; //<--ADDED by fduenas 20-06-2006
-  FIELD_TYPE_ENUM      = 247;
-  FIELD_TYPE_SET       = 248;
-  FIELD_TYPE_TINY_BLOB = 249;
-  FIELD_TYPE_MEDIUM_BLOB = 250;
-  FIELD_TYPE_LONG_BLOB = 251;
-  FIELD_TYPE_BLOB      = 252;
-  FIELD_TYPE_VAR_STRING = 253;
-  FIELD_TYPE_STRING    = 254;
-  FIELD_TYPE_GEOMETRY  = 255; }
-{ For Compatibility }
-{  FIELD_TYPE_CHAR      = FIELD_TYPE_TINY;
-  FIELD_TYPE_INTERVAL  = FIELD_TYPE_ENUM; }
-
   { Field's flags }
   NOT_NULL_FLAG          = 1;     { Field can't be NULL }
   PRI_KEY_FLAG           = 2;     { Field is part of a primary key }
