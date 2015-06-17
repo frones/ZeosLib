40c6f7238f80438b649c20577c6b26269b53a9c2
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2015-06-17 17:32:19 +0000

    fix broken compile
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.2@3635 d681b350-854c-439c-a81d-c0ce20710203

--- src/core/ZVariant.pas
+++ src/core/ZVariant.pas
@@ -2492,7 +2492,7 @@ DateTimeFromUnicode:
         vtAnsiString:
           Result.VUnicodeString := ZWideString(Value.VAnsiString);
         vtUTF8String:
-          Result.VUnicodeString := ZRawToUnicode(Pointer(Value.VUTF8String), Length(Value.VUTF8String), zCP_UTF8);
+          Result.VUnicodeString := PRawToUnicode(Pointer(Value.VUTF8String), Length(Value.VUTF8String), zCP_UTF8);
         vtRawByteString:
           Result.VUnicodeString := FConSettings^.ConvFuncs.ZRawToUnicode(Value.VRawByteString, FConSettings^.ClientCodePage^.CP);
         vtUnicodeString:
