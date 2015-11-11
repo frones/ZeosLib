a4e7a8e303552c5502d6cc27f497b9f0a1e76429
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2015-11-11 06:01:35 +0000

    add a Generic TODBCTokenizer which can be used by ADO and OledDB but is plattform portable
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@3656 d681b350-854c-439c-a81d-c0ce20710203

--- src/parsesql/ZOleDBToken.pas
+++ src/parsesql/ZOleDBToken.pas
@@ -3,7 +3,9 @@
 {                 Zeos Database Objects                   }
 {           String tokenizing classes for OleDB           }
 {                                                         }
-{            Originally written by EgonHugeist            }
+{         Originally written by Sergey Seroukhov          }
+{                                                         }
+{         unit Originally written by EgonHugeist          }
 {                                                         }
 {*********************************************************}
 
@@ -54,144 +56,16 @@ unit ZOleDBToken;
 interface
 
 {$I ZParseSql.inc}
-{$IFDEF ENABLE_OLEDB}
 
+{$IFDEF ENABLE_OLEDB}
 uses
-  Classes, SysUtils, ZTokenizer, ZGenericSqlToken, ZCompatibility;
+  ZODBCToken;
 
 type
-  {** Implements a quote string state object. }
-  TZOleDBQuoteState = class (TZQuoteState)
-  public
-    function NextToken(Stream: TStream; FirstChar: Char;
-      {%H-}Tokenizer: TZTokenizer): TZToken; override;
-
-    function EncodeString(const Value: string; QuoteChar: Char): string; override;
-    function DecodeString(const Value: string; QuoteChar: Char): string; override;
-  end;
-
   {** Implements a default tokenizer object. }
-  TZOleDBTokenizer = class (TZGenericSQLTokenizer)
-  protected
-    procedure CreateTokenStates; override;
-  end;
-
-implementation
-
-{ TZOleDBSQLQuoteState }
-
-{**
-  Return a quoted string token from a reader. This method
-  will collect characters until it sees a match to the
-  character that the tokenizer used to switch to this state.
-
-  @return a quoted string token from a reader
-}
-function TZOleDBQuoteState.NextToken(Stream: TStream; FirstChar: Char;
-  Tokenizer: TZTokenizer): TZToken;
-var
-  ReadChar: Char;
-  LastChar: Char;
-begin
-  Result.Value := FirstChar;
-  LastChar := #0;
-  while Stream.Read(ReadChar{%H-}, SizeOf(Char)) > 0 do
-  begin
-    if ((LastChar = FirstChar) and (ReadChar <> FirstChar)
-      and (FirstChar <> '[')) or ((FirstChar = '[') and (LastChar = ']')) then
-    begin
-      Stream.Seek(-SizeOf(Char), soFromCurrent);
-      Break;
-    end;
-    Result.Value := Result.Value + ReadChar;
-    if (LastChar = FirstChar) and (ReadChar = FirstChar) then
-      LastChar := #0
-    else LastChar := ReadChar;
-  end;
-
-  if CharInSet(FirstChar, ['"', '[']) then
-    Result.TokenType := ttQuotedIdentifier
-  else Result.TokenType := ttQuoted;
-end;
-
-{**
-  Encodes a string value.
-  @param Value a string value to be encoded.
-  @param QuoteChar a string quote character.
-  @returns an encoded string.
-}
-function TZOleDBQuoteState.EncodeString(const Value: string; QuoteChar: Char): string;
-begin
-  if QuoteChar = '[' then
-    Result := '[' + Value + ']'
-  else if CharInSet(QuoteChar, [#39, '"']) then
-    Result := QuoteChar + Value + QuoteChar
-  else Result := Value;
-end;
-
-{**
-  Decodes a string value.
-  @param Value a string value to be decoded.
-  @param QuoteChar a string quote character.
-  @returns an decoded string.
-}
-function TZOleDBQuoteState.DecodeString(const Value: string; QuoteChar: Char): string;
-begin
-  Result := Value;
-  if Length(Value) >= 2 then
-  begin
-    if CharInSet(QuoteChar, [#39, '"']) and (Value[1] = QuoteChar)
-      and (Value[Length(Value)] = QuoteChar) then
-    begin
-      if Length(Value) > 2 then
-        Result := AnsiDequotedStr(Value, QuoteChar)
-      else Result := '';
-    end
-    else if (QuoteChar = '[') and (Value[1] = QuoteChar)
-      and (Value[Length(Value)] = ']') then
-      Result := Copy(Value, 2, Length(Value) - 2)
-  end;
-end;
-
-
-{ TZOleDBTokenizer }
-
-{**
-  Constructs a default state table (as described in the class comment).
-}
-procedure TZOleDBTokenizer.CreateTokenStates;
-begin
-  EscapeState := TZEscapeState.Create;
-  NumberState := TZNumberState.Create;
-  QuoteState := TZOleDBQuoteState.Create;
-  WhitespaceState := TZWhitespaceState.Create;
-  CommentState := TZCppCommentState.Create;
-
-  SymbolState := TZGenericSQLSymbolState.Create;
-  WordState := TZGenericSQLWordState.Create;
-
-  SetCharacterState(#0, #32, WhitespaceState);
-  SetCharacterState(#33, #191, SymbolState);
-  SetCharacterState(#192, High(Char), WordState);
-
-  SetCharacterState('a', 'z', WordState);
-  SetCharacterState('A', 'Z', WordState);
-  SetCharacterState('_', '_', WordState);
-  SetCharacterState('$', '$', WordState);
-  SetCharacterState('@', '@', WordState);
-
-  SetCharacterState('0', '9', NumberState);
-  SetCharacterState('.', '.', NumberState);
-
-  SetCharacterState('"', '"', QuoteState);
-  SetCharacterState(#39, #39, QuoteState);
-  SetCharacterState('[', '[', QuoteState);
-  SetCharacterState(']', ']', QuoteState);
-
-  SetCharacterState('/', '/', CommentState);
-end;
-
-{$ELSE !ENABLE_OLEDB}
-implementation
+  TZOleDBTokenizer = TZODBCTokenizer;
 {$ENDIF ENABLE_OLEDB}
+
+implementation
+
 end.
