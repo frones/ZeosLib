d88519e6504ab2686e80370a82fe621d18df1d3d
marsupilami79 <marsupilami79@d681b350-854c-439c-a81d-c0ce20710203>
2017-06-09 16:47:36 +0000

    Reenable CAPIPreparedStatement on fpc / ARM
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.2@3994 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcPostgreSql.pas
+++ src/dbc/ZDbcPostgreSql.pas
@@ -826,11 +826,9 @@ begin
   Case GetTestMode of
     0:
   {$ENDIF}
-      {$IF NOT(DEFINED(FPC) and DEFINED(CPUARM))}
       if GetServerMajorVersion >= 8 then
         Result := TZPostgreSQLCAPIPreparedStatement.Create(GetPlainDriver, Self, '', Info)
       else
-      {$IFEND}
         Result := TZPostgreSQLClassicPreparedStatement.Create(GetPlainDriver, Self, '', Info);
   {$IFDEF ZEOS_TEST_ONLY}
     1: Result := TZPostgreSQLClassicPreparedStatement.Create(GetPlainDriver, Self, '', Info);
@@ -876,11 +874,9 @@ begin
   Case GetTestMode of
     0:
   {$ENDIF}
-      {$IF NOT(DEFINED(FPC) and DEFINED(CPUARM))}
       if GetServerMajorVersion >= 8 then
         Result := TZPostgreSQLCAPIPreparedStatement.Create(GetPlainDriver, Self, SQL, Info)
       else
-      {$IFEND}
         Result := TZPostgreSQLClassicPreparedStatement.Create(GetPlainDriver, Self, SQL, Info);
   {$IFDEF ZEOS_TEST_ONLY}
     1: Result := TZPostgreSQLClassicPreparedStatement.Create(GetPlainDriver, Self, SQL, Info);
