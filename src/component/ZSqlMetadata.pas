4711891e53b7b4ba7bf51afede26706925585d33
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2018-04-11 10:53:39 +0000

    7.2 last patch was wrong -> reset cursor override for the virtual-resultsets resolves the empty resultsets
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@4473 d681b350-854c-439c-a81d-c0ce20710203

--- src/component/ZSqlMetadata.pas
+++ src/component/ZSqlMetadata.pas
@@ -201,8 +201,6 @@ begin
       mdUserDefinedTypes:
         Result := Metadata.GetUDTs(FCatalog, FSchema, FTypeName, nil);
      end;
-     if Assigned(Result) then
-      Result.BeforeFirst;
   finally
     Connection.HideSQLHourGlass;
   end;
