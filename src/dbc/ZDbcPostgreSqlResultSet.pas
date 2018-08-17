9647af6b20bac5d612de6aadae3bcac022d82c18
marsupilami79 <marsupilami79@d681b350-854c-439c-a81d-c0ce20710203>
2018-08-17 15:41:53 +0000

    7.2: Add some ifdefs that reenable using infinity an neginfinity on FPC 2.6 with range checks enabled. Just using them without ifdefs will throw an compiler error: "Range check error while evaluating constants". Strange.
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@4963 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcPostgreSqlResultSet.pas
+++ src/dbc/ZDbcPostgreSqlResultSet.pas
@@ -197,6 +197,7 @@ uses
 // added for suporting Infinity, -Infinity and NaN.
 // See https://sourceforge.net/p/zeoslib/tickets/173/
 // maybe this should be pushed into ZSysUtils.SQLStrToFloatDef?
+{$IF defined(DELPHI) or defined(FPC_HAS_TYPE_EXTENDED)}
 procedure pgSQLStrToFloatDef(Value: PAnsiChar; const Def: Extended;
   var Result: Extended); overload;
 begin
@@ -209,10 +210,21 @@ begin
   else
     ZSysUtils.SQLStrToFloatDef(Value, Def, Result);
 end;
+{$IFEND}
 
 procedure pgSQLStrToFloatDef(Value: PAnsiChar; const Def: Single;
   var Result: Single); overload;
 begin
+  {$IFDEF FPC2_6DOWN}
+  {$ifopt R+}
+  {$define RangeCheckWasOn}
+  {$R-}
+  {$endif opt R+}
+  {$ifopt Q+}
+  {$define OverflowCheckWasOn}
+  {$Q-}
+  {$endif opt Q+}
+  {$ENDIF}
   if Value = 'Infinity' then
     Result := Infinity
   else if Value = '-Infinity' then
@@ -221,12 +233,31 @@ begin
     Result := NaN
   else
     ZSysUtils.SQLStrToFloatDef(Value, Def, Result);
+  {$IFDEF FPC2_6DOWN}
+  {$ifdef RangeCheckWasOn}
+  {$R+}
+  {$undef RangeCheckWasOn}
+  {$endif}
+  {$ifdef OverflowCheckWasOn}
+  {$Q+}
+  {$undef OverflowCheckWasOn}
+  {$endif}
+  {$ENDIF}
 end;
 
-{$IF defined(DELPHI) or defined(FPC_HAS_TYPE_EXTENDED)}
 procedure pgSQLStrToFloatDef(Value: PAnsiChar; const Def: Double;
   var Result: Double); overload;
 begin
+  {$IFDEF FPC2_6DOWN}
+  {$ifopt R+}
+  {$define RangeCheckWasOn}
+  {$R-}
+  {$endif opt R+}
+  {$ifopt Q+}
+  {$define OverflowCheckWasOn}
+  {$Q-}
+  {$endif opt Q+}
+  {$ENDIF}
   if Value = 'Infinity' then
     Result := Infinity
   else if Value = '-Infinity' then
@@ -235,8 +266,17 @@ begin
     Result := NaN
   else
     ZSysUtils.SQLStrToFloatDef(Value, Def, Result);
+  {$IFDEF FPC2_6DOWN}
+  {$ifdef RangeCheckWasOn}
+  {$R+}
+  {$undef RangeCheckWasOn}
+  {$endif}
+  {$ifdef OverflowCheckWasOn}
+  {$Q+}
+  {$undef OverflowCheckWasOn}
+  {$endif}
+  {$ENDIF}
 end;
-{$IFEND}
 
 { TZAbstractPostgreSQLStringResultSet }
 
