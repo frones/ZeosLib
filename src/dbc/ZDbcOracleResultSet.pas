dfc0b9f74f27dfaaece324a0034cb041e597f3a7
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2018-10-11 17:42:25 +0000

    fix last commit -> typo
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@5177 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcOracleResultSet.pas
+++ src/dbc/ZDbcOracleResultSet.pas
@@ -1547,7 +1547,7 @@ begin
             nvuNegInf: Result := NegInfinity;
             nvuPosInf: Result := Infinity;
             {$IFDEF RangeCheckEnabled}{$R+}{$ENDIF}
-            {$IFDEF OverFlowCheckEnabled}}{$Q+}{$ENDIF}
+            {$IFDEF OverFlowCheckEnabled}{$Q+}{$ENDIF}
             vnuNegInt: Result := NegNvu2Int(POCINumber(P), FvnuInfo);
 {$IF defined (RangeCheckEnabled) and defined(WITH_UINT64_C1118_ERROR)}{$R-}{$IFEND}
             vnuPosInt: Result := PosNvu2Int(POCINumber(P), FvnuInfo);
