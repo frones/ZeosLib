5b51f23a38a9b44b24b235021db374e529483f92
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2019-02-09 10:53:51 +0000

    7.2* fix last commit again
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@5461 d681b350-854c-439c-a81d-c0ce20710203

--- src/parsesql/ZPostgreSqlToken.pas
+++ src/parsesql/ZPostgreSqlToken.pas
@@ -217,16 +217,18 @@ var
 begin
   Result := inherited NextToken(SPos, NTerm, Tokenizer);
   //detecting Postgre Parameters as one ttWordState:
-  if (Result.P^ = '$') and (SPos < NTerm) and ((SPos+1)^ <> '$') then begin
-    Inc(SPos);
-    if Tokenizer.WordState <> nil then
-    WordToken := Tokenizer.WordState.NextToken(SPos, NTerm, Tokenizer);
-    if (WordToken.TokenType = ttWord) and (SPos^ <> '$') then begin
-      Result.TokenType := ttWord;
-      Result.L := Result.L+WordToken.L;
-      Exit;
-    end else
-      SPos := Result.P;
+  if (Result.P^ = '$') and (SPos < NTerm) then begin
+    if ((SPos+1)^ <> '$') then begin
+      Inc(SPos);
+      if Tokenizer.WordState <> nil then
+      WordToken := Tokenizer.WordState.NextToken(SPos, NTerm, Tokenizer);
+      if (WordToken.TokenType = ttWord) and (SPos^ <> '$') then begin
+        Result.TokenType := ttWord;
+        Result.L := Result.L+WordToken.L;
+        Exit;
+      end else
+        SPos := Result.P;
+    end;
     //detect body tags as ttQuoted
     //eg. $body$ .... $body$ or $$ .... $$
     DollarCount := 1;
