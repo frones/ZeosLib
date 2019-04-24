534072b8bd17f7cee9937e028af2af494e78af21
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2019-04-24 19:16:48 +0000

    7.2* DBLIB determine the Date-Formats for SQLServer also if not FreeTDS
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@5600 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcDbLib.pas
+++ src/dbc/ZDbcDbLib.pas
@@ -453,18 +453,13 @@ begin
       ConSettings^.ClientCodePage^.CP := zCP_UTF8;
       ConSettings^.ClientCodePage^.Encoding := ceUTF8;
       ConSettings^.ClientCodePage^.IsStringFieldCPConsistent := True;
-    end
-    else
-    begin
+    end else begin
       ConSettings^.ClientCodePage^.CP := FServerAnsiCodePage;
       ConSettings^.ClientCodePage^.IsStringFieldCPConsistent := False;
     end;
     ConSettings^.AutoEncode := True; //Must be set because we can't determine a column-codepage! e.g NCHAR vs. CHAR Fields
     SetConvertFunctions(ConSettings);
-    DetermineMSDateFormat;
-  end
-  else
-  begin
+  end else begin
     if (FProvider = dpSybase) and (not FreeTDS)
     then ConSettings^.ClientCodePage^.IsStringFieldCPConsistent := False;
     FServerAnsiCodePage := ConSettings^.ClientCodePage^.CP;
@@ -475,8 +470,9 @@ begin
   http://technet.microsoft.com/en-us/library/ms180878%28v=sql.105%29.aspx
    Using DATE and DATETIME in ISO 8601 format is multi-language supported:
    DATE Un-separated
-   DATETIME as YYYY-MM-DDTHH:NN:SS
-  }
+   DATETIME as YYYY-MM-DDTHH:NN:SS }
+  if (FProvider = dpMsSQL) then
+    DetermineMSDateFormat;
   ConSettings^.WriteFormatSettings.DateFormat := 'YYYYMMDD';
   ConSettings^.WriteFormatSettings.DateTimeFormat := 'YYYY-MM-DDTHH:NN:SS';
   SetDateTimeFormatProperties(False);
