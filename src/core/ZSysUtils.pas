23bccc8334e1318fff279fec8f29d9e669e80033
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2019-08-03 04:58:46 +0000

    Merged revision(s) 5753-5755 from branches/testing-7.3: Convert documentation comments to xmldoc and move them to the interface declaration, where applicable ........ Add documentation for IZStatement. Comments are copied from ZDbcStatement.pas. Some modifications have been done - we don't talk about JDBC anymore and null was replaced by nil ........ 7.2* wrong CP conversion and FPC hints
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/7.2-patches@5799 d681b350-854c-439c-a81d-c0ce20710203

--- src/core/ZSysUtils.pas
+++ src/core/ZSysUtils.pas
@@ -1095,7 +1095,6 @@ implementation
 uses DateUtils, StrUtils,
   {$IF defined(WITH_RTLCONSTS_SInvalidGuidArray) or defined(TLIST_IS_DEPRECATED)}RTLConsts,{$IFEND}
   SysConst,{keep it after RTLConst -> deprecated warning}
-  {$IFDEF WITH_DBCONSTS}DBConsts{$ENDIF},
   ZFastCode;
 
 
