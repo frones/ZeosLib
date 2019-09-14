d353cd3956511a3164f4153a72a62ae7aff803f1
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2019-09-14 14:13:58 +0000

    Cardinal2PG for Endian_big
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@5923 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcPostgreSqlUtils.pas
+++ src/dbc/ZDbcPostgreSqlUtils.pas
@@ -1224,9 +1224,7 @@ begin
   {$ENDIF}
 {$ELSE}
 begin
-  //2019-09-13 Jan: Reverse4 bytes is only used in other functions when they are little endian.
-  //I assume, it is used wrongly here.
-  //Reverse4Bytes(Buf)
+  PCardinal(Buf)^ := Value;
 {$ENDIF}
 end;
 
