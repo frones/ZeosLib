frones/ZeosLib

commit 81c7f66fa00a9efe888f80fc950494ff1d5a23aa

use the SQLWriters some more...
git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@5942 d681b350-854c-439c-a81d-c0ce20710203

src/dbc/ZDbcInterbase6Statement.pas
@@ -1301,6 +1301,7 @@ end;
   @param parameterIndex the first parameter is 1, the second is 2, ...
   @param x the parameter value
 }
+{$IF defined (RangeCheckEnabled) and defined(WITH_UINT64_C1118_ERROR)}{$R-}{$IFEND}
 procedure TZInterbase6PreparedStatement.AddParamLogValue(ParamIndex: Integer;
   SQLWriter: TZRawSQLStringWriter; var Result: RawByteString);
 var XSQLVAR: PXSQLVAR;
@@ -1312,7 +1313,7 @@ begin
   CheckParameterIndex(ParamIndex);
   {$R-}
   XSQLVAR := @FParamXSQLDA.sqlvar[ParamIndex];
-  {$IFDEF RangeCheckEnabled} {$R+} {$ENDIF}
+  {$IF definde(RangeCheckEnabled and not defined(WITH_UINT64_C1118_ERROR)}{$R+} {$ENDIF}
   if (XSQLVAR.sqlind <> nil) and (XSQLVAR.sqlind^ = ISC_NULL) then
     Result := '(NULL)'
   else case XSQLVAR.sqltype and not(1) of
@@ -1385,6 +1386,7 @@ begin
     else            SQLWriter.AddText('(UNKNOWN)', Result);
   end;
 end;
+{$IF defined (RangeCheckEnabled) and defined(WITH_UINT64_C1118_ERROR)}{$R+}{$IFEND}
 
 procedure TZInterbase6PreparedStatement.SetInt(Index, Value: Integer);
 var XSQLVAR: PXSQLVAR;

src/core/ZClasses.pas
@@ -324,6 +324,7 @@ type
     procedure AddOrd(Value: Integer;        var Result: RawByteString); overload;
     procedure AddOrd(const Value: UInt64;   var Result: RawByteString); overload;
     procedure AddOrd(const Value: Int64;    var Result: RawByteString); overload;
+    procedure AddOrd(Value: Pointer;        var Result: RawByteString); overload;
     procedure AddFloat(Value: Single;       var Result: RawByteString); overload;
     procedure AddFloat(const Value: Double; var Result: RawByteString); overload;
     procedure AddDecimal(const Value: Currency;   var Result: RawByteString); overload;
@@ -335,6 +336,8 @@ type
     procedure Finalize(var Result: RawByteString);
     procedure CancelLastComma(var Result: RawByteString);
     procedure CancelLastCharIfExists(Value: AnsiChar; var Result: RawByteString);
+    procedure ReplaceOrAddLastChar(OldChar, NewChar: AnsiChar; var Result: RawByteString);
+    procedure AddLineFeedIfNotEmpty(var Result: RawByteString);
   end;
 
   {** EH: implements a buffered UTF16 encoded writer }
@@ -376,9 +379,13 @@ type
     procedure Finalize(var Result: UnicodeString);
     procedure CancelLastComma(var Result: UnicodeString);
     procedure CancelLastCharIfExists(Value: WideChar; var Result: UnicodeString);
+    procedure ReplaceOrAddLastChar(OldChar, NewChar: WideChar; var Result: UnicodeString);
+    procedure AddLineFeedIfNotEmpty(var Result: UnicodeString);
   end;
 
   TZSQLStringWriter = {$IFDEF UNICODE}TZUnicodeSQLStringWriter{$ELSE}TZRawSQLStringWriter{$ENDIF};
+  SQLString = {$IFDEF UNICODE}UnicodeString{$ELSE}RawByteString{$ENDIF};
+
 implementation
 
 uses ZMessages, ZFastCode
@@ -714,6 +721,12 @@ begin
   AddHexBinary(Pointer(Value), Length(Value), ODBC, Result);
 end;
 
+procedure TZRawSQLStringWriter.AddLineFeedIfNotEmpty(var Result: RawByteString);
+begin
+  if (Pointer(Result) <> nil) or (FPos > FBuf) then
+    AddText(LineEnding, Result);
+end;
+
 procedure TZRawSQLStringWriter.AddChar(Value: AnsiChar; var Result: RawByteString);
 var P: PAnsiChar;
 begin
@@ -840,6 +853,30 @@ begin
   GetMem(FBuf, AnsiCharCapacity);
   FPos := FBuf;
   FEnd := FBuf+AnsiCharCapacity;
+end;
+
+procedure TZRawSQLStringWriter.ReplaceOrAddLastChar(OldChar, NewChar: AnsiChar;
+  var Result: RawByteString);
+var P: PAnsichar;
+  L: LengthInt;
+  label setp;
+begin
+  if (FPos > FBuf) then begin
+    P := FPos-1;
+    if PByte(P)^ = Byte(OldChar)
+    then PByte(P)^ := Ord(NewChar)
+    else goto setp
+  end else begin
+    P := Pointer(Result);
+    L := Length(Result)-{$IFDEF WITH_TBYTES_AS_RAWBYTESTRING}2{$ELSE}1{$ENDIF};
+    Inc(P, L);
+    if (L >= 1) and (PByte(P)^ = Byte(OldChar)) then
+      PByte(P)^ := Byte(NewChar)
+    else begin
+setp: PByte(FPos)^ := Byte(NewChar);
+      Inc(FPos);
+    end;
+  end;
 end;
 
 procedure TZRawSQLStringWriter.AddOrd32(Value: Cardinal; Digits: Byte;
@@ -1119,7 +1156,7 @@ end;
 procedure TZRawSQLStringWriter.AddText(const AsciiValue: UnicodeString;
   var Result: RawByteString);
 var PW: PWidechar;
-  PA: PAnsiChar
+  PA: PAnsiChar;
   L: LengthInt;
 begin
   PW := Pointer(AsciiValue);
@@ -1138,6 +1175,12 @@ begin
   end;
 end;
 {$ENDIF}
+
+procedure TZRawSQLStringWriter.AddOrd(Value: Pointer;
+  var Result: RawByteString);
+begin
+  AddOrd({$IFDEF CPU64}UInt64{$ELSE}Cardinal{$ENDIF}(Value), Result);
+end;
 
 { TZUnicodeSQLStringWriter }
 
@@ -1290,6 +1333,13 @@ procedure TZUnicodeSQLStringWriter.AddHexBinary(const Value: TBytes;
   ODBC: Boolean; var Result: UnicodeString);
 begin
   AddHexBinary(Pointer(Value), Length(Value), ODBC, Result);
+end;
+
+procedure TZUnicodeSQLStringWriter.AddLineFeedIfNotEmpty(
+  var Result: UnicodeString);
+begin
+  if (Pointer(Result) <> nil) or (FPos > FBuf) then
+    AddText(LineEnding, Result);
 end;
 
 procedure TZUnicodeSQLStringWriter.AddOrd(Value: ShortInt;
@@ -1517,6 +1567,30 @@ begin
   GetMem(FBuf, WideCharCapacity shl 1);
   FPos := FBuf;
   FEnd := FBuf+WideCharCapacity;
+end;
+
+procedure TZUnicodeSQLStringWriter.ReplaceOrAddLastChar(OldChar,
+  NewChar: WideChar; var Result: UnicodeString);
+var P: PWidechar;
+  L: LengthInt;
+  label setp;
+begin
+  if (FPos > FBuf) then begin
+    P := FPos-1;
+    if PWord(P)^ = Ord(OldChar)
+    then PWord(P)^ := Ord(NewChar)
+    else goto setp
+  end else begin
+    P := Pointer(Result);
+    L := Length(Result)-{$IFDEF WITH_TBYTES_AS_RAWBYTESTRING}2{$ELSE}1{$ENDIF};
+    Inc(P, L);
+    if (L >= 1) and (PWord(P)^ = Ord(OldChar)) then
+      PWord(P)^ := Ord(NewChar)
+    else begin
+setp: PWord(FPos)^ := Ord(NewChar);
+      Inc(FPos);
+    end;
+  end;
 end;
 
 end.

src/core/ZFastCode.pas
@@ -306,28 +306,28 @@ procedure IntToUnicode(const Value: UInt64; Buf: PWideChar; PEnd: ZPPWideChar = 
 procedure IntToUnicode(Value: Cardinal; Buf: PWideChar; Digits: Byte); overload; {$IFDEF WITH_INLINE}inline;{$ENDIF}
 procedure IntToUnicode(Value: UInt64; Buf: PWideChar; Digits: Byte); overload;
 
-function IntToUnicode(Value: Byte): ZWideString; overload;
-function IntToUnicode(Value: ShortInt): ZWideString; overload;
-function IntToUnicode(Value: Word): ZWideString; overload;
-function IntToUnicode(Value: SmallInt): ZWideString; overload;
-function IntToUnicode(Value: Cardinal): ZWideString; overload;
-function IntToUnicode(Value: Integer): ZWideString; overload;
-function IntToUnicode(const Value: Int64): ZWideString; overload;
-function IntToUnicode(const Value: UInt64): ZWideString; overload;
+function IntToUnicode(Value: Byte): UnicodeString; overload;
+function IntToUnicode(Value: ShortInt): UnicodeString; overload;
+function IntToUnicode(Value: Word): UnicodeString; overload;
+function IntToUnicode(Value: SmallInt): UnicodeString; overload;
+function IntToUnicode(Value: Cardinal): UnicodeString; overload;
+function IntToUnicode(Value: Integer): UnicodeString; overload;
+function IntToUnicode(const Value: Int64): UnicodeString; overload;
+function IntToUnicode(const Value: UInt64): UnicodeString; overload;
 
 procedure CurrToRaw(const Value: Currency; Buf: PAnsiChar; PEnd: PPAnsiChar = nil); overload;
 function CurrToRaw(const Value: Currency): RawByteString; overload;
 
 procedure CurrToUnicode(const Value: Currency; Buf: PWideChar; PEnd: ZPPWideChar = nil); overload;
-function CurrToUnicode(const Value: Currency): ZWideString; overload;
+function CurrToUnicode(const Value: Currency): UnicodeString; overload;
 
 function RawToInt(const Value: RawByteString): Integer; overload;
 function RawToInt(const Value: PAnsiChar): Integer; overload;
 function RawToInt64(const Value: RawByteString): Int64;
 function RawToUInt64(const Value: RawByteString): UInt64;
-function UnicodeToInt(const Value: ZWideString): Integer;
-function UnicodeToInt64(const Value: ZWideString): Int64;
-function UnicodeToUInt64(const Value: ZWideString): UInt64;
+function UnicodeToInt(const Value: UnicodeString): Integer;
+function UnicodeToInt64(const Value: UnicodeString): Int64;
+function UnicodeToUInt64(const Value: UnicodeString): UInt64;
 
 function RawToIntDef(const S: RawByteString; const Default: Integer) : Integer; overload;
 function RawToIntDef(const S: PAnsiChar; const Default: Integer) : Integer; overload;
@@ -339,13 +339,13 @@ function RawToUInt64Def(const S: PAnsiChar; const Default: UInt64) : UInt64; ove
 function RawToUInt64Def(Buf, PEnd: PAnsiChar; const Default: UInt64) : UInt64; overload;
 function RawToUInt64Def(const S: RawByteString; const Default: UInt64) : UInt64; overload;
 
-function UnicodeToIntDef(const S: ZWideString; const Default: Integer) : Integer; overload;
+function UnicodeToIntDef(const S: UnicodeString; const Default: Integer) : Integer; overload;
 function UnicodeToIntDef(const S: PWideChar; const Default: Integer) : Integer; overload;
 function UnicodeToIntDef(Buf, PEnd: PWideChar; Default: Integer) : Integer; overload;
-function UnicodeToInt64Def(const S: ZWideString; const Default: Int64) : Int64; overload;
+function UnicodeToInt64Def(const S: UnicodeString; const Default: Int64) : Int64; overload;
 function UnicodeToInt64Def(const S: PWideChar; const Default: Int64) : Int64; overload;
 function UnicodeToInt64Def(Buf, PEnd: PWideChar; Default: Integer) : Int64; overload;
-function UnicodeToUInt64Def(const S: ZWideString; const Default: UInt64) : UInt64; overload;
+function UnicodeToUInt64Def(const S: UnicodeString; const Default: UInt64) : UInt64; overload;
 function UnicodeToUInt64Def(const S: PWideChar; const Default: UInt64) : UInt64; overload;
 function UnicodeToUInt64Def(Buf, PEnd: PWideChar; Default: Integer) : UInt64; overload;
 
@@ -408,15 +408,15 @@ function Trunc(const X: Single): Int64; overload;
 {$ENDIF USE_FAST_TRUNC}
 
 function Pos(const SubStr: RawByteString; const Str: RawByteString): Integer; overload;
-function Pos(const SubStr, Str: ZWideString): Integer; overload;
+function Pos(const SubStr, Str: UnicodeString): Integer; overload;
 
 function PosEx(const SubStr: RawByteString; Str: PAnsiChar; len: LengthInt; Offset: Integer = 1): Integer; overload;
 function PosEx(SubStr, Str: PAnsiChar; SubStrLen, Strlen: LengthInt; Offset: Integer = 1): Integer; overload;
 function PosEx(const SubStr, S: RawByteString; Offset: Integer = 1): Integer; overload;
 
-function PosEx(const SubStr: ZWideString; Str: PWideChar; len: LengthInt; Offset: Integer = 1): Integer; overload;
+function PosEx(const SubStr: UnicodeString; Str: PWideChar; len: LengthInt; Offset: Integer = 1): Integer; overload;
 function PosEx(SubStr, Str: PWideChar; SubStrLen, Strlen: LengthInt; Offset: Integer = 1): Integer; overload;
-function PosEx(const SubStr, S: ZWideString; Offset: Integer = 1): Integer; overload;
+function PosEx(const SubStr, S: UnicodeString; Offset: Integer = 1): Integer; overload;
 
 function GetOrdinalDigits(const Value: UInt64): Byte; overload; {$IFDEF WITH_INLINE} inline;{$ENDIF}
 {$IFDEF HAVE_TRUE_NATIVE_TYPES}//some newer delphi's can't determine the correct overload ):
@@ -3299,7 +3299,7 @@ begin
   else PWord(Buf)^ := Ord(#0);
 end;
 
-function CurrToUnicode(const Value: Currency): ZWideString;
+function CurrToUnicode(const Value: Currency): UnicodeString;
 var buf: array[0..31] of WideChar;
   P: PWideChar;
 begin
@@ -3438,27 +3438,27 @@ cardinal_range:
   else PWord(Buf)^ := Word(I32 or ord('0'));
 end;
 
-function IntToUnicode(Value: Byte): ZWideString;
+function IntToUnicode(Value: Byte): UnicodeString;
 begin
   Result := IntToUnicode(Cardinal(Value));
 end;
 
-function IntToUnicode(Value: ShortInt): ZWideString;
+function IntToUnicode(Value: ShortInt): UnicodeString;
 begin
   Result := IntToUnicode(Integer(Value));
 end;
 
-function IntToUnicode(Value: Word): ZWideString;
+function IntToUnicode(Value: Word): UnicodeString;
 begin
   Result := IntToUnicode(Cardinal(Value));
 end;
 
-function IntToUnicode(Value: SmallInt): ZWideString;
+function IntToUnicode(Value: SmallInt): UnicodeString;
 begin
   Result := IntToUnicode(Integer(Value));
 end;
 
-function IntToUnicode(Value: Cardinal): ZWideString;
+function IntToUnicode(Value: Cardinal): UnicodeString;
 var
   Digits: Byte;
 begin
@@ -3467,7 +3467,7 @@ begin
   IntToUnicode(Value, Pointer(Result), Digits);
 end;
 
-function IntToUnicode(Value: Integer): ZWideString;
+function IntToUnicode(Value: Integer): UnicodeString;
 var C: Cardinal;
   Digits: Byte;
   Negative: Boolean;
@@ -3481,7 +3481,7 @@ begin
   IntToUnicode(C, P+Ord(Negative), Digits);
 end;
 
-function IntToUnicode(const Value: Int64): ZWideString;
+function IntToUnicode(const Value: Int64): UnicodeString;
 var U: UInt64;
   Digits: Byte;
   Negative: Boolean;
@@ -3495,7 +3495,7 @@ begin
   IntToUnicode(U, P+Ord(Negative), Digits);
 end;
 
-function IntToUnicode(const Value: UInt64): ZWideString;
+function IntToUnicode(const Value: UInt64): UnicodeString;
 var Digits: Byte;
 begin
   Digits := GetOrdinalDigits(Value);
@@ -3915,7 +3915,7 @@ end;
 {$WARNINGS ON}
 
 {$WARNINGS OFF}
-function UnicodeToInt(const Value: ZWideString): Integer;
+function UnicodeToInt(const Value: UnicodeString): Integer;
 //function StrToInt32_JOH_PAS_7_c(const s: string): Integer;
 //originally wrtten by John O'Harrow
 //http://fastcode.sourceforge.net/
@@ -4315,7 +4315,7 @@ begin
 end;
 {$WARNINGS ON}
 
-function UnicodeToIntDef(const S: ZWideString; const Default: Integer) : Integer;
+function UnicodeToIntDef(const S: UnicodeString; const Default: Integer) : Integer;
 var
   E: Integer;
 begin
@@ -4992,7 +4992,7 @@ begin
 end;
 {$WARNINGS ON}
 
-function UnicodeToInt64Def(const S: ZWideString; const Default: Int64) : Int64;
+function UnicodeToInt64Def(const S: UnicodeString; const Default: Int64) : Int64;
 var
   E: Integer;
 begin
@@ -5002,7 +5002,7 @@ begin
       Result := Default;
 end;
 
-function UnicodeToInt64(const Value: ZWideString) : Int64;
+function UnicodeToInt64(const Value: UnicodeString) : Int64;
 var
   E: Integer;
 begin
@@ -5030,7 +5030,7 @@ begin
     Result := Default;
 end;
 
-function UnicodeToUInt64(const Value: ZWideString) : UInt64;
+function UnicodeToUInt64(const Value: UnicodeString) : UInt64;
 var
   E: Integer;
 begin
@@ -5039,7 +5039,7 @@ begin
     raise EConvertError.CreateResFmt(@SInvalidInteger, [Value]);
 end;
 
-function UnicodeToUInt64Def(const S: ZWideString; const Default: UInt64) : UInt64;
+function UnicodeToUInt64Def(const S: UnicodeString; const Default: UInt64) : UInt64;
 var
   E: Integer;
 begin
@@ -7465,9 +7465,9 @@ end;
 //Match - [esp+$10]
 
 //function PosUnicode32_DKC_IA32_3_a(const SubStr, Str: UnicodeString): Integer; overload;
-//changes by EgonHugeist: ZWideString might be a WideString(D7-D007, old FPC)
+//changes by EgonHugeist: UnicodeString might be a WideString(D7-D007, old FPC)
 //where length address returns size in bytes -> shrink to codepoints
-function Pos(const SubStr, Str: ZWideString): Integer; overload;
+function Pos(const SubStr, Str: UnicodeString): Integer; overload;
 asm
  push  ebx
  push  esi
@@ -7751,7 +7751,7 @@ Ret:
 //Author:            Dennis Kjaer Christensen
 //Date:              18/07 2012
 //faster than Delphi and FPC RTL
-function Pos(const SubStr, Str: ZWideString): Integer;
+function Pos(const SubStr, Str: UnicodeString): Integer;
 {$IFDEF ZERO_BASED_STRINGS}
 begin
   Result := PosEx(SubStr, Pointer(Str), Length(Str));
@@ -7877,14 +7877,14 @@ end;
 
 // from Aleksandr Sharahov's PosEx_Sha_Pas_2()
 // changed to a unicode pointer version
-function PosEx(const SubStr: ZWideString; Str: PWideChar; len: LengthInt; Offset: Integer = 1): Integer;
+function PosEx(const SubStr: UnicodeString; Str: PWideChar; len: LengthInt; Offset: Integer = 1): Integer;
 begin;
   Result := PosEx(Pointer(SubStr), Str, Length(SubStr), len, OffSet);
 end;
 
 // from Aleksandr Sharahov's PosEx_Sha_Pas_2()
 // changed to a unicode version
-function PosEx(const SubStr, S: ZWideString; Offset: Integer = 1): Integer;
+function PosEx(const SubStr, S: UnicodeString; Offset: Integer = 1): Integer;
 begin;
   Result := PosEx(SubStr, Pointer(S), Length(S), Offset);
 end;

src/core/ZSysUtils.pas
@@ -112,6 +112,7 @@ const
   {$IFNDEF WITH_TBYTES_AS_RAWBYTESTRING}
   BoolStrIntsRaw: array[Boolean] of RawByteString = ('0', '1');
   BoolStrsRaw: array[Boolean] of RawByteString = (RawByteString(StrFalse), RawByteString(StrTrue));
+  YesNoStrsRaw: array[Boolean] of RawByteString = ('NO', 'YES');
   {$ENDIF}
   BoolStrs: array[Boolean] of string = (StrFalse, StrTrue);
   BoolStrsW: array[Boolean] of ZWideString = (ZWideString(StrFalse), ZWideString(StrTrue));
@@ -125,6 +126,7 @@ var
   {$IFDEF WITH_TBYTES_AS_RAWBYTESTRING} //can not be initialized ...
   BoolStrIntsRaw: array[Boolean] of RawByteString;
   BoolStrsRaw: array[Boolean] of RawByteString;
+  YesNoStrsRaw: array[Boolean] of RawByteString;
   {$ENDIF}
 
 {**
@@ -7467,6 +7469,7 @@ begin
   for B := False to True do begin
     BoolStrIntsRaw[B] := UnicodeStringToASCII7(BoolStrInts[B]);
     BoolStrsRaw[B] := UnicodeStringToASCII7(BoolStrsW[B]);
+    YesNoStrsRaw[B] := UnicodeStringToASCII7(YesNoStrs[b]);
   end;
 end;
 {$ENDIF}

src/core/ZTokenizer.pas
@@ -1268,6 +1268,9 @@ begin
   Result := TZTokenList.Create;
   LastTokenType := ttUnknown;
 
+  if Result.FCapacity < (EOS-Buffer) shr 5 then
+    Result.SetCapacity((EOS-Buffer) shr 5);
+
   while Buffer < EOS do begin
     State := FCharacterStates[Ord(Buffer^)];
     if State <> nil then

src/dbc/ZDbcASAMetadata.pas
@@ -1622,6 +1622,7 @@ function TZASADatabaseMetadata.UncachedGetColumns(const Catalog: string;
   const SchemaPattern: string; const TableNamePattern: string;
   const ColumnNamePattern: string): IZResultSet;
 var Len: NativeUInt;
+  P: PAnsiChar;
   function ObjectToSearchString(const S, LikePattern: String): String;
   begin
     if S='' then
@@ -1660,7 +1661,7 @@ begin
       Result.UpdateInt(TableColColumnOrdPosIndex, GetIntByName('ORDINAL_POSITION'));
       Result.UpdatePAnsiChar(TableColColumnIsNullableIndex, GetPAnsiCharByName('IS_NULLABLE', Len), Len);
       Result.UpdateBoolean(TableColColumnAutoIncIndex,
-        CompareText( Trim(GetStringByName('COLUMN_DEF')), 'autoincrement') = 0 );
+        CompareText(Trim(GetStringByName('COLUMN_DEF')), 'autoincrement') = 0 );
       Result.UpdateBoolean(TableColColumnCaseSensitiveIndex, IC.IsCaseSensitive(GetStringByName('COLUMN_NAME')));
       Result.UpdateBoolean(TableColColumnSearchableIndex, False);
       Result.UpdateBoolean(TableColColumnWritableIndex, True);
@@ -1678,17 +1679,19 @@ begin
         'c.column_name like '+ObjectToSearchString(ColumnNamePattern, '''%''')+' and c.column_type=''C'' '+
         'order by USER_NAME(t.creator) asc,t.table_name asc,c.column_id asc') do
   begin
-    while Next do
-    begin
+    while Next do begin
       while Result.GetInt(TableColColumnOrdPosIndex) <> GetIntByName( 'column_id') do
         Result.Next;
       Result.UpdateBoolean(TableColColumnWritableIndex, False);
       Result.UpdateBoolean(TableColColumnDefinitelyWritableIndex, False);
       Result.UpdateBoolean(TableColColumnReadonlyIndex, True);
-      if GetRawByteStringByName( 'nulls') = 'N' then
-      begin
-        Result.UpdateSmall(TableColColumnNullableIndex, 0);
-        Result.UpdateRawByteString(TableColColumnIsNullableIndex, 'NO');
+      P := GetPAnsiCharByName('nulls', Len);
+      if (P <> nil) and (Len = 1) and (PByte(P)^ = Ord('N')) then begin
+        Result.UpdateSmall(TableColColumnNullableIndex, Ord(ntNoNulls));
+        Result.UpdateRawByteString(TableColColumnIsNullableIndex, YesNoStrsRaw[False]);
+      end else begin
+        Result.UpdateSmall(TableColColumnNullableIndex, Ord(ntNullable));
+        Result.UpdateRawByteString(TableColColumnIsNullableIndex, YesNoStrsRaw[False]);
       end;
       Result.UpdateRow;
     end;

src/dbc/ZDbcASAResultSet.pas
@@ -1318,7 +1318,7 @@ end;
 }
 procedure TZASAAbstractResultSet.AfterClose;
 begin
-  FCursorName := '';
+  FCursorName := EmptyRaw;
   inherited AfterClose;
 end;
 
@@ -1334,7 +1334,7 @@ end;
 }
 procedure TZASAAbstractResultSet.ResetCursor;
 begin
-  if FCursorName <> '' then
+  if FCursorName <> EmptyRaw then
     FPLainDriver.dbpp_close(FASAConnection.GetDBHandle, Pointer(FCursorName));
   inherited ResetCursor;
 end;
@@ -1461,7 +1461,7 @@ begin
   FPlainDriver.dbpp_fetch(FASAConnection.GetDBHandle,
     Pointer(FCursorName), CUR_RELATIVE, Rows, FSqlData.GetData, BlockSize, CUR_FORREGULAR);
     ZDbcASAUtils.CheckASAError(FPlainDriver,
-      FASAConnection.GetDBHandle, lcOther, ConSettings, '', SQLE_CURSOR_NOT_OPEN); //handle a known null resultset issue (cursor not open)
+      FASAConnection.GetDBHandle, lcOther, ConSettings, EmptyRaw, SQLE_CURSOR_NOT_OPEN); //handle a known null resultset issue (cursor not open)
   if FASAConnection.GetDBHandle.sqlCode = SQLE_CURSOR_NOT_OPEN then Exit;
   if FASAConnection.GetDBHandle.sqlCode <> SQLE_NOTFOUND then begin
     //if (RowNo > 0) or (RowNo + Rows < 0) then

src/dbc/ZDbcASAStatement.pas
@@ -161,7 +161,13 @@ begin
   FPlainDriver := TZASAPlainDriver(FASAConnection.GetIZPlainDriver.GetInstance);
   FetchSize := BlockSize;
   ResultSetType := rtScrollSensitive;
-  CursorName := IntToRaw(NativeUInt(FASAConnection.GetDBHandle))+'_'+IntToRaw(FStatementId);
+  with ZClasses.TZRawSQLStringWriter.Create(40) do begin
+    AddOrd(Pointer(FASAConnection.GetDBHandle), FCursorName);
+    AddChar(AnsiChar('_'), FCursorName);
+    AddOrd(FStatementId, FCursorName);
+    Finalize(FCursorName);
+    Free;
+  end;
 end;
 
 destructor TZAbstractASAStatement.Destroy;
@@ -302,7 +308,7 @@ begin
       ZDbcASAUtils.CheckASAError(FPlainDriver, GetDBHandle, lcExecute, ConSettings);
       if GetDBHandle.sqlcode = SQLE_PROCEDURE_COMPLETE
       then Result := false
-      else DescribeCursor(FASAConnection, FSQLData, CursorName, '');
+      else DescribeCursor(FASAConnection, FSQLData, CursorName, EmptyRaw);
     end;
   end;
 end;
@@ -648,7 +654,7 @@ begin
   CheckParameterIndex(ParamIndex);
   SQLVAR := @FInParamSQLDA.sqlvar[ParamIndex];
   if (SQLVar.sqlInd <> nil) and (SQLVar.sqlInd^ = -1) then
-    Result := '(NULL)'
+    SQLWriter.AddText('(NULL)', Result)
   else case SQLVar.sqlType and $FFFE of
     DT_SMALLINT         : SQLWriter.AddOrd(PSmallInt(SQLVAR.sqlData)^, Result);
     DT_INT              : SQLWriter.AddOrd(PInteger(SQLVAR.sqlData)^, Result);

commit 1d6833b673559bc8914c890d4237444d48fc6aa2

fix some typos introduced in R5942
git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@5944 d681b350-854c-439c-a81d-c0ce20710203

src/dbc/ZDbcInterbase6Statement.pas
@@ -1313,7 +1313,7 @@ begin
   CheckParameterIndex(ParamIndex);
   {$R-}
   XSQLVAR := @FParamXSQLDA.sqlvar[ParamIndex];
-  {$IF definde(RangeCheckEnabled and not defined(WITH_UINT64_C1118_ERROR)}{$R+} {$ENDIF}
+  {$IF defined(RangeCheckEnabled) and not defined(WITH_UINT64_C1118_ERROR)}{$R+} {$ENDIF}
   if (XSQLVAR.sqlind <> nil) and (XSQLVAR.sqlind^ = ISC_NULL) then
     Result := '(NULL)'
   else case XSQLVAR.sqltype and not(1) of