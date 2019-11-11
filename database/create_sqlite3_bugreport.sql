572c4809292c4e75ee85d041640013d4db4b6797
marsupilami79 <marsupilami79@d681b350-854c-439c-a81d-c0ce20710203>
2019-11-11 13:32:10 +0000

    7.2* SQLite: resolve ticket #386 by emulating the pattern matches in UncachedGetColumns
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/trunk@6109 d681b350-854c-439c-a81d-c0ce20710203

---
+++ database/create_sqlite3_bugreport.sql
@@ -0,0 +1,9 @@
+/*==============================================================*/
+/* Tables for Ticket #386                                       */
+/*==============================================================*/
+
+CREATE TABLE table_ticket_386 (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  Field_1 Varchar(10),
+  Field_2 Int
+);
