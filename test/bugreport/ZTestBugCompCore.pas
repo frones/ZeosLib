b89b4ed0674361fab665ff5a3872ee8a7c338034
marsupilami79 <marsupilami79@d681b350-854c-439c-a81d-c0ce20710203>
2020-03-05 13:27:11 +0000

    Modify the test for ticket 418
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@6351 d681b350-854c-439c-a81d-c0ce20710203

--- test/bugreport/ZTestBugCompCore.pas
+++ test/bugreport/ZTestBugCompCore.pas
@@ -1865,18 +1865,18 @@ begin
   Connection.Connect;
   try
     Connection.ExecuteDirect('delete from number_values where n_id in (1001, 1002, 1003, 1003, 1004, 1005)');
-    Connection.ExecuteDirect('insert into number_values (n_id, n_float) values (1001, 0.0)');
-    Connection.ExecuteDirect('insert into number_values (n_id, n_float) values (1002, 1.3376)');
-    Connection.ExecuteDirect('insert into number_values (n_id, n_float) values (1003, 0.8246)');
-    Connection.ExecuteDirect('insert into number_values (n_id, n_float) values (1004, 0.8459)');
-    Connection.ExecuteDirect('insert into number_values (n_id, n_float) values (1005, 0.5684)');
+    Connection.ExecuteDirect('insert into number_values (n_id, n_numeric) values (1001, 0.0)');
+    Connection.ExecuteDirect('insert into number_values (n_id, n_numeric) values (1002, 1.3376)');
+    Connection.ExecuteDirect('insert into number_values (n_id, n_numeric) values (1003, 0.8246)');
+    Connection.ExecuteDirect('insert into number_values (n_id, n_numeric) values (1004, 0.8459)');
+    Connection.ExecuteDirect('insert into number_values (n_id, n_numeric) values (1005, 0.5684)');
 
     Query := TZReadOnlyQuery.Create(nil);
     try
       Query.Connection := Connection;
-      Query.SQL.Text := 'select n_id, n_float from number_values where n_id in (1001, 1002, 1003, 1004, 1005) order by n_id';
+      Query.SQL.Text := 'select n_id, n_numeric from number_values where n_id in (1001, 1002, 1003, 1004, 1005) order by n_id';
       Query.Open;
-      Query.SortedFields := 'n_float';
+      Query.SortedFields := 'n_numeric';
 
       Query.First;
       CheckEquals(1001, Query.FieldByName('n_id').AsInteger);
@@ -1904,18 +1904,18 @@ begin
   Connection.Connect;
   try
     Connection.ExecuteDirect('delete from number_values where n_id in (1001, 1002, 1003, 1003, 1004, 1005)');
-    Connection.ExecuteDirect('insert into number_values (n_id, n_float) values (1001, 0.0)');
-    Connection.ExecuteDirect('insert into number_values (n_id, n_float) values (1002, 1.3376)');
-    Connection.ExecuteDirect('insert into number_values (n_id, n_float) values (1003, 0.8246)');
-    Connection.ExecuteDirect('insert into number_values (n_id, n_float) values (1004, 0.8459)');
-    Connection.ExecuteDirect('insert into number_values (n_id, n_float) values (1005, 0.5684)');
+    Connection.ExecuteDirect('insert into number_values (n_id, n_numeric) values (1001, 0.0)');
+    Connection.ExecuteDirect('insert into number_values (n_id, n_numeric) values (1002, 1.3376)');
+    Connection.ExecuteDirect('insert into number_values (n_id, n_numeric) values (1003, 0.8246)');
+    Connection.ExecuteDirect('insert into number_values (n_id, n_numeric) values (1004, 0.8459)');
+    Connection.ExecuteDirect('insert into number_values (n_id, n_numeric) values (1005, 0.5684)');
 
     Query := TZReadOnlyQuery.Create(nil);
     try
       Query.Connection := Connection;
-      Query.SQL.Text := 'select n_id, n_float from number_values where n_id in (1001, 1002, 1003, 1004, 1005) order by n_id';
+      Query.SQL.Text := 'select n_id, n_numeric from number_values where n_id in (1001, 1002, 1003, 1004, 1005) order by n_id';
       Query.Open;
-      Query.IndexFieldNames := 'n_float';
+      Query.IndexFieldNames := 'n_numeric';
 
       Query.First;
       CheckEquals(1001, Query.FieldByName('n_id').AsInteger);
