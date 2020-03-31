ad5ef03c02418d39c8eabc5fc9c448ed8f89daf9
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2020-03-31 15:12:54 +0000

    fix update regression reported by Miab3 thanks for the test
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@6396 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcGenericResolver.pas
+++ src/dbc/ZDbcGenericResolver.pas
@@ -374,7 +374,7 @@ label CopyParams;
 begin
   { Use precached values. }
   WhereColumnsLookup.Clear;
-  if FWhereColumns.Count > 0 then
+  if (FWhereColumns.Count > 0) then
     goto CopyParams;
   Table := '';
   { Defines catalog, schema and a table. }
@@ -412,9 +412,10 @@ begin
     end;
   end;
 
-  if FWhereColumns.Count >= 0 then
-    FillWhereAllColumns(IncrementDestIndexBy)
-  else begin
+  if FWhereColumns.Count = 0 then begin
+    WhereAll := True;
+    FillWhereAllColumns(IncrementDestIndexBy);
+  end else begin
 CopyParams:
     WhereColumnsLookup.Capacity := FWhereColumns.Count;
     for I := 0 to FWhereColumns.Count -1 do begin
@@ -477,7 +478,7 @@ procedure TZGenerateSQLCachedResolver.FormWhereClause(
   const SQLWriter: TZSQLStringWriter; const OldRowAccessor: TZRowAccessor;
   var Result: SQLString);
 var
-  I, N, IDX: Integer;
+  I, N, J, IDX: Integer;
   Condition: SQLString;
 begin
   N := 0;
@@ -493,6 +494,8 @@ begin
     if OldRowAccessor.IsNull(IDX) then begin
       SQLWriter.AddText(' IS NULL', Result);
       FCurrentWhereColumns.Delete(N);
+      for IDX := N to FCurrentWhereColumns.Count -1 do
+        Dec(PZIndexPair(FCurrentWhereColumns[IDX]).SrcOrDestIndex);
     end else begin
       SQLWriter.AddText('=?', Result);
       Inc(N);
