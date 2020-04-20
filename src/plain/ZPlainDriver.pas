d288833f2304457b5f8e2716c5c62d59e73207b4
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2020-04-20 15:31:28 +0000

    Merged revision(s) 6411, 6417 from branches/testing-7.3: 7.2* fixing codepages to the alias was broken ........ 7.2* resolve some nextgen warnings ........
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/7.2-patches@6462 d681b350-854c-439c-a81d-c0ce20710203

--- src/plain/ZPlainDriver.pas
+++ src/plain/ZPlainDriver.pas
@@ -166,7 +166,7 @@ function TZAbstractPlainDriver.ValidateCharEncoding(const CharacterSetName: Stri
 begin
   Result := GetClientCodePageInformations(CharacterSetName);
   if (DoArrange) and (Result^.ZAlias <> '' ) then
-    ValidateCharEncoding(Result^.ZAlias); //recalls em selves
+    Result := ValidateCharEncoding(Result^.ZAlias); //recalls em selves
 end;
 
 {**
