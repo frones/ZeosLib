2cece194efe0ec1498d1b9f483cec1c1eb441c6b
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2020-09-30 06:56:19 +0000

    adding testcase for ticket #437
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/testing-7.3@6875 d681b350-854c-439c-a81d-c0ce20710203

--- database/create_oracle_bugreport.sql
+++ database/create_oracle_bugreport.sql
@@ -11,6 +11,24 @@ Num		   NUMBER
 
 INSERT INTO Table_Num1 VALUES(1, 54321.0123456789);
 
+create table table_ticket437(
+  id int not null,
+  text varchar2(4000),
+  clobek clob,
+  longek long
+);
+
+insert into table_ticket437(id, text)
+select 1, 'ASD' from dual
+union all
+select 2, 'ASDF' from dual
+union all
+select 3, 'QWERT' from dual;
+
+update table_ticket437 set clobek = text;
+
+update table_ticket437 set longek = clobek;
+
 SET TERM ^ ;
 CREATE OR REPLACE PACKAGE P_TICKET96 AS
   PROCEDURE Update_Doc(id NUMBER, qty NUMBER, price NUMBER);
