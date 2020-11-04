4590480f246c4cae009f4db4aa31e8f4f7eff122
egonhugeist <egonhugeist@d681b350-854c-439c-a81d-c0ce20710203>
2020-11-04 17:37:32 +0000

    Merged revision(s) 7013, 7017, 7019, 7011 from trunk: [PATCH] #218 - Why to manually determine absolute path if the OS can do that for us? Also, only one method to override if backwards compatibility is an issue. Credit to MJFShark. ........ AEHimself: [PATCH] Eh... ExpandFileName is not as smart as I thought it is. Let's bring back some removed methods and reinforce the LoadLibraryEx logic. ........ fix Android compilation ........
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/7.2-patches@7037 d681b350-854c-439c-a81d-c0ce20710203

--- src/plain/ZPlainLoader.pas
+++ src/plain/ZPlainLoader.pas
@@ -97,7 +97,7 @@ type
 
 implementation
 
-uses SysUtils, 
+uses SysUtils,
 {$IFDEF MSWINDOWS}
   Windows,
 (*{$ELSE}
@@ -119,7 +119,7 @@ var
 begin
   SetLength(FLocations, Length(Locations));
   for I := 0 to High(Locations) do
-    FLocations[I] := Locations[I]; 
+    FLocations[I] := Locations[I];
   FHandle := INVALID_HANDLE_VALUE;
   FCurrentLocation := '';
   FLoaded := False;
@@ -172,37 +172,50 @@ begin
 end;
 
 function TZNativeLibraryLoader.ZLoadLibrary(const Location: String): Boolean;
-var newpath, temp: String; // AB modif
+var newpath{$IF not declared(LoadLibraryEx)}, temp{$IFEND}: String; // AB modif
 begin
   if FLoaded then
     Self.FreeNativeLibrary;
-  temp := ''; //init for FPC
   FLoaded := False;
   Result := False;
   newpath := ExtractFilePath(Location);
   // AB modif BEGIN
+{$IF not declared(LoadLibraryEx)}
+  temp := ''; //init for FPC
   try
-   if newpath <> '' then begin
-     temp := GetCurrentDir;
-     SetCurrentDir(newpath);
-   end;
+    if newpath <> '' then begin
+      temp := GetCurrentDir;
+      SetCurrentDir(newpath);
+    end;
+{$IFEND}
   // AB modif END
 
 {$IFDEF UNIX}
   {$IFDEF FPC}
     FHandle := LoadLibrary(PAnsiChar(Location));
-  {$ELSE}
+  {$ELSE} //Kylix
     FHandle := HMODULE(dlopen(PAnsiChar(Location), RTLD_GLOBAL));
   {$ENDIF}
 {$ELSE}
+  {$IF declared(LoadLibraryEx)} //windows only
+  // So the thing is... if we don't specify a library location, we only get a dll name here. Like 'oci.dll'
+  // ExpandFileName is so "smart" that it consideres it a relative path and expands it to appear like it's right
+  // next to the application. If the DLL is in the search path, we'll not be able to find it. So let's check if
+  // a path was provided, and if yes, then use LOAD_WITH_ALTERED_SEARCH_PATH. If no, fall back to the "original"
+  // version and let Windows find the DLL somewhere.
+  If newpath <> '' Then FHandle := LoadLibraryEx(PChar(ExpandFileName(Location)), 0, LOAD_WITH_ALTERED_SEARCH_PATH)
+    Else FHandle := LoadLibraryEx(PChar(Location), 0, 0);
+  {$ELSE !LoadLibraryEx}
   FHandle := LoadLibrary(PChar(Location));
+  {$IFEND !LoadLibraryEx}
 {$ENDIF}
-
+{$IF not declared(LoadLibraryEx)}
   // AB modif BEGIN
   finally
-   if temp<>'' then
-     SetCurrentDir(temp);
+    if temp<>'' then
+      SetCurrentDir(temp);
   end;
+{$IFEND !LoadLibraryEx}
   // AB modif END
   if (FHandle <> INVALID_HANDLE_VALUE) and (FHandle <> 0) then
   begin
@@ -273,6 +286,3 @@ begin
 end;
 
 end.
-
-
-
