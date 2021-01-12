08dc7c6015bc07fbb804df6be7a8c1ae9a42c554
marsupilami79 <marsupilami79@d681b350-854c-439c-a81d-c0ce20710203>
2021-01-12 19:48:20 +0000

    Change the Zeos 7.2 version to Zeos 7.2.10
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/7.2-patches@7218 d681b350-854c-439c-a81d-c0ce20710203

--- src/core/ZClasses.pas
+++ src/core/ZClasses.pas
@@ -63,7 +63,7 @@ uses
 const
   ZEOS_MAJOR_VERSION = 7;
   ZEOS_MINOR_VERSION = 2;
-  ZEOS_SUB_VERSION = 8;
+  ZEOS_SUB_VERSION = 10;
   ZEOS_STATUS = 'release';
   ZEOS_VERSION = Char(48+ZEOS_MAJOR_VERSION)+'.'+
                  Char(48+ZEOS_MINOR_VERSION)+'.'+
