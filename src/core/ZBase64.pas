5859ea7ad5b4bf7d650f54fff8ad75a578a67be5
marsupilami79 <marsupilami79@d681b350-854c-439c-a81d-c0ce20710203>
2021-05-21 07:27:02 +0000

    Add Base64 implementations for IDEs without NetEncoding unit.
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/8.0-patches@7549 d681b350-854c-439c-a81d-c0ce20710203

--- src/core/ZBase64.pas
+++ src/core/ZBase64.pas
@@ -4,31 +4,135 @@ unit ZBase64;
 
 interface
 
-{$IFDEF WITH_NETENCODING}
+{$IF DEFINED(FPC) OR DEFINED(WITH_TBYTES)}
 
 uses
-  Classes, SysUtils, System.NetEncoding;
+  Classes, SysUtils;
 
-function DecodeBase64(const InStr: AnsiString): TBytes;
-function EncodeBase64(const InValue: TBytes): String;
+function ZDecodeBase64(const InStr: AnsiString): TBytes;
+function ZEncodeBase64(const InValue: TBytes): AnsiString;
 
-{$ENDIF}
+{$IFEND}
 
 implementation
 
-{$IFDEF WITH_NETENCODING}
+{$IF DEFINED(FPC) OR DEFINED(WITH_TBYTES)}
 
-function DecodeBase64(const InStr: AnsiString): TBytes;
-begin
-  Result := TNetEncoding.Base64.DecodeStringToBytes(InStr);
-end;
+uses {$IFDEF WITH_NETENCODING}
+     System.NetEncoding
+     {$ELSE}
+       {$IFDEF FPC}
+       Base64
+       {$ELSE}
+       EncdDecd
+       {$ENDIF}
+     {$ENDIF};
 
-function EncodeBase64(const InValue: TBytes): String;
-begin
-  Result := String(TNetEncoding.Base64.EncodeBytesToString(InValue));
-end;
-
+function ZDecodeBase64(const InStr: AnsiString): TBytes;
+{$IFNDEF WITH_NETENCODING}
+var
+  {$IFDEF FPC}
+  InStream: TStringStream;
+  DecodingStream: TBase64DecodingStream;
+  OutStream: TBytesStream;
+  {$ELSE}
+  InStream: TMemoryStream;
+  OutStream: TBytesStream;
+  {$ENDIF}
 {$ENDIF}
+begin
+  {$IFDEF WITH_NETENCODING}
+  Result := TNetEncoding.Base64.DecodeStringToBytes(InStr);
+  {$ELSE}
+    {$IFDEF FPC}
+    try
+      InStream := TStringStream.Create(InStr);
+      DecodingStream := TBase64DecodingStream.Create(InStream);
+      OutStream := TBytesStream.Create;
+
+      OutStream.CopyFrom(DecodingStream, DecodingStream.Size);
+      Result := OutStream.Bytes;
+    finally
+      if Assigned(OutStream) then
+        FreeAndNil(OutStream);
+      if Assigned(DecodingStream) then
+        FreeAndNil(DecodingStream);
+      if Assigned(InStream) then
+        FreeAndNil(InStream);
+    end;
+    {$ELSE}
+    try
+      InStream := TMemoryStream.Create;
+      OutStream := TBytesStream.Create;
+
+      InStream.Write(InStr[1], Length(InStr));
+      InStream.Position := 0;
+
+      DecodeStream(InStream, OutStream);
+
+      Result := OutStream.Bytes;
+    finally
+      if Assigned(InStream) then
+        FreeAndNil(InStream);
+      if Assigned(OutStream) then
+        FreeAndNil(OutStream);
+    end;
+    {$ENDIF}
+  {$ENDIF}
+end;
+
+function ZEncodeBase64(const InValue: TBytes): AnsiString;
+{$IFNDEF WITH_NETENCODING}
+var
+  {$IFDEF FPC}
+  InStream: TBytesStream;
+  EncodingStream: TBase64EncodingStream;
+  OutStream: TStringStream;
+  {$ELSE}
+  InStream: TBytesStream;
+  OutStream: TMemoryStream;
+  {$ENDIF}
+{$ENDIF}
+begin
+  {$IFDEF WITH_NETENCODING}
+  Result := String(TNetEncoding.Base64.EncodeBytesToString(InValue));
+  {$ELSE}
+    {$IFDEF FPC}
+    try
+      InStream := TBytesStream.Create(InValue);
+      EncodingStream := TBase64EncodingStream.Create(InStream);
+      OutStream := TStringStream.Create('');
+
+      OutStream.CopyFrom(EncodingStream, EncodingStream.Size);
+      Result := OutStream.DataString;
+    finally
+      if Assigned(OutStream) then
+        FreeAndNil(OutStream);
+      if Assigned(EncodingStream) then
+        FreeAndNil(EncodingStream);
+      if Assigned(InStream) then
+        FreeAndNil(InStream);
+    end;
+    {$ELSE}
+    try
+      InStream := TBytesStream.Create(InValue);
+      OutStream := TMemoryStream.Create;
+
+      EncodeStream(InStream, OutStream);
+
+      SetLength(Result, OutStream.Size);
+      Move(OutStream.Memory^, Result[1], OutStream.Size);
+    finally
+      if Assigned(InStream) then
+        FreeAndNil(InStream);
+      if Assigned(OutStream) then
+        FreeAndNil(OutStream);
+    end;
+    {$ENDIF}
+  {$ENDIF}
+end;
+
+{$IFEND}
 
 end.
 
