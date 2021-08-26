f55f6de99ad511a196d7016040ef1ffd0dd1eda1
marsupilami79 <marsupilami79@d681b350-854c-439c-a81d-c0ce20710203>
2021-08-26 09:23:54 +0000

    Merged revision(s) 7647 from branches/8.0-patches: Fix a case where Firebird Interface API doesn't detect a connection loss because we didn't check if there were any errors. ........
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/trunk@7648 d681b350-854c-439c-a81d-c0ce20710203

--- src/dbc/ZDbcFirebirdInterbase.pas
+++ src/dbc/ZDbcFirebirdInterbase.pas
@@ -1362,6 +1362,7 @@ procedure BindSQLDAInParameters(BindList: TZBindList;
 const
   sCS_NONE = 'NONE';
   DS_Props_IsMetadataResultSet = 'IsMetadataResultSet';
+  sGetTxn = RawByteString('GET NEW TRANSACTION');
   sStartTxn = RawByteString('TRANSACTION STARTED.');
   sCommitMsg = RawByteString('TRANSACTION COMMIT');
   sRollbackMsg = RawByteString('TRANSACTION ROLLBACK');
