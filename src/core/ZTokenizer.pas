37c9f66016db410f80de78324d536a96b2397c37
marsupilami79 <marsupilami79@d681b350-854c-439c-a81d-c0ce20710203>
2022-01-15 13:19:21 +0000

    Merged revision(s) 7759-7760 from trunk: TZTokenizer doesn't recognize -- comments ........ by aehimself: Fix for failing tests after TZTokenizer --comment fix ........
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/8.0-patches@7761 d681b350-854c-439c-a81d-c0ce20710203

--- src/core/ZTokenizer.pas
+++ src/core/ZTokenizer.pas
@@ -575,7 +575,9 @@ begin
   { Gets a token result. }
   if not GotAdigit then begin
     SPos := Result.P;
-    if Tokenizer.SymbolState <> nil then
+    if (SPos^ = '-') And (Tokenizer.CommentState <> nil) then
+      Result := Tokenizer.CommentState.NextToken(SPos, NTerm, Tokenizer)
+    else if Tokenizer.SymbolState <> nil then
       Result := Tokenizer.SymbolState.NextToken(SPos, NTerm, Tokenizer);
   end else begin
     if AbsorbedDot
@@ -715,6 +717,8 @@ end;
 }
 function TZCppCommentState.NextToken(var SPos: PChar; const NTerm: PChar;
   Tokenizer: TZTokenizer): TZToken;
+Var
+ len: Integer;
 begin
   Result.TokenType := ttUnknown;
   Result.P := SPos;
@@ -734,6 +738,10 @@ begin
           Result.TokenType := ttComment;
           GetSingleLineComment(SPos, NTerm);
           Result.L := SPos-Result.P+1;
+
+          len := Length(Result.P);
+          If Result.L > len Then
+            Result.L := len;
           Exit;
         end;
       else
