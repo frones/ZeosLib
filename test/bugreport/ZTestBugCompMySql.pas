716049ef4343f4cd130341664d793011d23406d8
marsupilami79 <marsupilami79@d681b350-854c-439c-a81d-c0ce20710203>
2024-04-29 08:31:11 +0000

    Add a tgest for https://zeoslib.sourceforge.io/viewtopic.php?t=199899
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/8.0-patches@8138 d681b350-854c-439c-a81d-c0ce20710203

--- test/bugreport/ZTestBugCompMySql.pas
+++ test/bugreport/ZTestBugCompMySql.pas
@@ -124,13 +124,15 @@ type
     procedure TestBigIntError;
     procedure TestBCD_Refresh_p156227;
     procedure TestTicket265;
+    //test for https://zeoslib.sourceforge.io/viewtopic.php?t=199899
+    procedure TestForum199899;
   end;
 
 {$ENDIF ZEOS_DISABLE_MYSQL}
 implementation
 {$IFNDEF ZEOS_DISABLE_MYSQL}
 
-uses ZTestCase, ZDbcMySQL, ZSysUtils, ZDbcProperties, ZDbcMySqlMetadata;
+uses ZTestCase, ZDbcMySQL, ZSysUtils, ZDbcProperties, ZDbcMySqlMetadata, ZStoredProcedure;
 
 { TZTestCompMySQLBugReport }
 
@@ -1996,6 +1998,26 @@ begin
     FreeAndNil(tbl);
   end;
 end;
+
+procedure TZTestCompMySQLBugReport.TestForum199899;
+var
+  Proc: TZStoredProc;
+begin
+  Proc := TZStoredProc.Create(nil);
+  try
+    Proc.Connection := Connection;
+    Proc.StoredProcName := 'forum199899';
+    Proc.ParamByName('somevalue').AsString := 'Test';
+    Proc.Open;
+
+    CheckEquals(1, Proc.RecordCount, 'Record Count');
+    Check(not Proc.Fields[0].IsNull, 'Result must not be null');
+    CheckEquals(0, Proc.Fields[0].AsInteger, 'String length');
+  finally
+    FreeAndNil(Proc);
+  end;
+end;
+
 
 procedure TZTestCompMySQLBugReport.TestTicket304;
 var
