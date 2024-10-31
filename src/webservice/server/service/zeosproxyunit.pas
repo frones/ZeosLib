7ee6ad0636af924216c70e3276c97c434663a4ee
marsupilami79 <marsupilami79@d681b350-854c-439c-a81d-c0ce20710203>
2024-10-31 09:24:43 +0000

    Some fixes to the zeos proxy service implementation, so it conforms to the changes in the code base.
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/8.0-patches@8264 d681b350-854c-439c-a81d-c0ce20710203

--- src/webservice/server/service/zeosproxyunit.pas
+++ src/webservice/server/service/zeosproxyunit.pas
@@ -55,7 +55,7 @@ unit zeosproxyunit;
 interface
 
 uses
-  Classes, SysUtils, DaemonApp, server_listener, {$IFDEF WITH_DNSSD}mdnsService, {$ENDIF}
+  Classes, SysUtils, DaemonApp, server_listener, dbcproxyconfigstore, {$IFDEF WITH_DNSSD}mdnsService, {$ENDIF}
   // for including the Zeos drivers:
   ZDbcAdo, ZDbcASA, ZDbcDbLib, ZDbcFirebird, ZDbcInterbase6, ZDbcMySql, ZDbcODBCCon,
   ZDbcOleDB, ZDbcOracle, ZDbcPostgreSql, ZDbcSQLAnywhere, ZDbcSqLite, ZDbcProxyMgmtDriver,
@@ -108,13 +108,13 @@ end;
 procedure TZeosProxyDaemon.DataModuleStart(Sender: TCustomDaemon;
   var OK: Boolean);
 var
-  configFile: String;
+  ConfigFile: String;
 begin
   {$IFDEF LINUX}
-  configFile := '/etc/zeosproxy.ini';
+  ConfigFile := '/etc/zeosproxy.ini';
   zeosproxy_imp.Logger := TDbcProxyConsoleLogger.Create;
   {$ELSE}
-  configFile := ExtractFilePath(ParamStr(0)) + 'zeosproxy.ini';
+  ConfigFile := ExtractFilePath(ParamStr(0)) + 'zeosproxy.ini';
   zeosproxy_imp.Logger := TDbcProxyFileLogger.Create(ExtractFilePath(ParamStr(0)) + 'zeosproxy.log');
   {$ENDIF}
 
@@ -130,13 +130,13 @@ begin
   InitializeSSLLibs;
 
 
-  ConfigManager := TDbcProxyConfigManager.Create;
-  ConfigManager.LoadBaseConfig(configFile);
+  ConfigManager := TDbcProxyIniConfigManager.Create(ConfigFile) as IZDbcProxyConfigStore;
+  ConfigManager.LoadBaseConfig;
   if ConfigManager.LogFile <> '' then begin
     FreeAndNil(zeosproxy_imp.Logger);
     zeosproxy_imp.Logger := TDbcProxyFileLogger.Create(ConfigManager.LogFile);
   end;
-  ConfigManager.LoadConnectionConfig(configFile);
+  ConfigManager.LoadConnectionConfig;
 
   try
     InitTofuCerts;
