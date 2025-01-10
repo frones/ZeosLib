91d8ea0bb3e2da453754a4b24eaf0dcab1e274c7
marsupilami79 <marsupilami79@d681b350-854c-439c-a81d-c0ce20710203>
2025-01-10 08:43:16 +0000

    Add a missing unit for DuckDB
    git-svn-id: http://svn.code.sf.net/p/zeoslib/code-0/branches/8.0-patches@8295 d681b350-854c-439c-a81d-c0ce20710203

---
+++ src/dbc/ZDbcDuckDBUtils.pas
@@ -0,0 +1,33 @@
+unit ZDbcDuckDBUtils;
+
+{$I zdbc.inc}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+{$IFNDEF ZEOS_DISABLE_DUCKDB} //if set we have an empty unit
+
+const
+  DuckDBMicrosecondsPerDay = 24 * 60 *60 *1000 * 1000;
+
+var
+  //the encoded date of 1970-01-01
+  DuckDBDateShift: Integer;
+
+{$ENDIF ZEOS_DISABLE_DUCKDB} //if set we have an empty unit
+implementation
+{$IFNDEF ZEOS_DISABLE_DUCKDB} //if set we have an empty unit
+
+uses
+  DateUtils;
+
+initialization
+  DuckDBDateShift := Trunc(EncodeDate(1970, 1, 1));
+  //DuckDBMicrosecondsPerDay = 24 * 60 *60 *1000 * 1000;
+
+{$ENDIF ZEOS_DISABLE_DUCKDB} //if set we have an empty unit
+
+end.
+
